11a05efc019db205ea3e22157a02e23826765d79
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-03-19 00:55:49 +0530

    Time: 18 ms (7.51%), Space: 52 MB (5.26%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
@@ -0,0 +1,67 @@
+
+/*
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+        return solve(arr, 0, arr.length-1).minSum;
+    }
+    
+    public Pair solve(int[] arr, int low, int high){
+        if (low == high)
+            return new Pair(arr[low], 0);
+        Pair root = new Pair(0, Integer.MAX_VALUE);
+        
+        for (int i=low; i<high; i++){
+            Pair leftPair = solve(arr, low, i);
+            Pair rightPair = solve(arr, i+1, high);
+            
+            int sum = leftPair.minSum + rightPair.minSum + rightPair.maxLeaf*leftPair.maxLeaf;
+            if (sum < root.minSum){
+                root.maxLeaf = Integer.max(rightPair.maxLeaf, leftPair.maxLeaf);
+                root.minSum = sum;
+            }
+        }
+        return root;
+    }
+}
+*/
+    
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+        return solve(arr, 0, arr.length-1, new Pair[arr.length][arr.length]).minSum;
+    }
+    
+    public Pair solve(int[] arr, int low, int high, Pair[][] dp){
+        if (low == high)
+            return new Pair(arr[low], 0);
+        Pair root = new Pair(0, Integer.MAX_VALUE);
+        
+        if (dp[low][high]!=null)
+            return dp[low][high];
+        
+        for (int i=low; i<high; i++){
+            Pair leftPair = solve(arr, low, i, dp);
+            Pair rightPair = solve(arr, i+1, high, dp);
+            
+            int sum = leftPair.minSum + rightPair.minSum + rightPair.maxLeaf*leftPair.maxLeaf;
+            if (sum < root.minSum){
+                root.maxLeaf = Integer.max(rightPair.maxLeaf, leftPair.maxLeaf);
+                root.minSum = sum;
+            }
+        }
+        
+        dp[low][high] = root;
+        return root;
+    }
+}
+
+
+
+class Pair{
+    int maxLeaf;
+    int minSum;
+    public Pair(int maxLeaf, int minSum){
+        this.minSum = minSum;
+        this.maxLeaf = maxLeaf;
+        
+    }
+}
