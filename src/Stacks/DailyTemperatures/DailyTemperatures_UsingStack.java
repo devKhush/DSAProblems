db6d6b91f5a5855bd9dbadff434e56460ee52968
Khushdev Pandit <khushdev20211@iiitd.ac.in>
2022-03-19 23:51:47 +0530

    daily temp...

--- src/Stacks/DailyTemperatures/DailyTemperatures_UsingStack.java
+++ src/Stacks/DailyTemperatures/DailyTemperatures_UsingStack.java
@@ -16,7 +16,9 @@ class DailyTemperatures_UsingStack {
             }
             stack.push(i);
         }
-        
+
+        // This below 2 lines are actually not needed bcoz, in array default elments are always 0
+        // So it remains 0 if such a day is not found
         while (!stack.isEmpty())
             answer[stack.pop()] = 0;
         return answer;
--- src/Stacks/DailyTemperatures/DailyTemperatures_WithoutStack.java
+++ src/Stacks/DailyTemperatures/DailyTemperatures_WithoutStack.java
@@ -1,4 +1,10 @@
-class Solution {
+package Stacks.DailyTemperatures;
+
+// Based on simple logic
+// DRY Run once, O(n^2) in worst case
+// 95% faster than stack one
+
+public class DailyTemperatures_WithoutStack {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] answer = new int[temperatures.length];
         int maxIndex = 0;
