97cd86f7fe435abf535e9d89c7f8574139033a94
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-03-20 19:57:01 +0530

    Time: 1 ms (99.78%), Space: 44.4 MB (35.01%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,24 +1,44 @@
 class Solution {
-    public int[] finalPrices(int[] prices) {
-        // int[] answer = new int[prices.length];
-        Stack <Integer> stack = new Stack<>();
+//     public int[] finalPrices(int[] prices) {
+//         // int[] answer = new int[prices.length];
+//         Stack <Integer> stack = new Stack<>();
         
-        for (int i=0; i<prices.length; i++){
+//         for (int i=0; i<prices.length; i++){
             
-            while(!stack.isEmpty()  &&  prices[i] <= prices[stack.peek()]){
-                int index =  stack.pop();
-                prices[index] = prices[index] - prices[i];
-            }
+//             while(!stack.isEmpty()  &&  prices[i] <= prices[stack.peek()]){
+//                 int index =  stack.pop();
+//                 prices[index] = prices[index] - prices[i];
+//             }
             
-            stack.push(i);
-        }
-        
-//         while (!stack.isEmpty()){
-//             int index = stack.pop();
-//             answer[index] = prices[index];
+//             stack.push(i);
 //         }
         
-        // return answer;
+// //         while (!stack.isEmpty()){
+// //             int index = stack.pop();
+// //             answer[index] = prices[index];
+// //         }
+        
+//         // return answer;
+//         return prices;
+//     }
+    
+    public int[] finalPrices(int[] prices) {
+        
+        for (int i=0; i<prices.length; i++){
+            int j=i+1;
+            
+            while(j<prices.length){
+                if (prices[j] <= prices[i]){
+                    prices[i] -= prices[j];
+                    break;
+                }
+                j++;
+            }
+            
+        }
         return prices;
+        
     }
 }
+
+
