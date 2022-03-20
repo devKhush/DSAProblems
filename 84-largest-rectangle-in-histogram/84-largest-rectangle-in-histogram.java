f6d0abd4d8c7c2a32d5279c1efcf06204a561b4a
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-03-21 00:34:24 +0530

    Time: 202 ms (27.54%), Space: 54.1 MB (89.90%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,51 +1,5 @@
 class Solution {
     
-    
-    public int largestRectangleArea_123(int[] heights) {
-        int n = heights.length;
-        int[] nextSmallerElement = new int[n];
-        int[] prevSmallerElement = new int[n];
-        
-        for (int i=0; i<n; i++){
-            int j = i;
-            while (j < n){
-                if (heights[i] > heights[j]){
-                    nextSmallerElement[i] = j;
-                    break;
-                }
-                j++;
-                if (j==n)
-                    nextSmallerElement[i] = n;
-            }
-        }
-        
-        for (int i=0; i<n; i++){
-            int j = i;
-            while (j >= 0){
-                if (heights[j] < heights[i]){
-                    prevSmallerElement[i] = j;
-                    break;
-                }
-                j--;    
-                if (j==-1)
-                    prevSmallerElement[i] = -1;
-            }
-        }
-        
-//         System.out.println(Arrays.toString(nextSmallerElement));
-//         System.out.println(Arrays.toString(prevSmallerElement));
-        
-        int maxArea = 0;
-        for (int i=0; i<heights.length; i++){
-            int area = (nextSmallerElement[i] - prevSmallerElement[i] -1) * heights[i];
-            maxArea = Math.max(maxArea, area);
-        }
-        
-        return maxArea;
-        
-    }
-    
-    
     public int largestRectangleArea(int[] heights) {
         int[] nextSmallerElement = nextSmallerElementIndices(heights, heights.length);
         int[] prevSmallerElement = previousSmallerElementIndices(heights, heights.length);
