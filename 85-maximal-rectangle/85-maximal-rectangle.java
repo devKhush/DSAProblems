9e5bba8da17977d559d19e3d009d41f52736c764
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-03-21 21:15:14 +0530

    Time: 6 ms (91.37%), Space: 55.2 MB (20.06%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.java
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -21,85 +21,35 @@ class Solution {
         return maxArea;
     }
     
-    public int maximumAreaInHistogram(char[] heights) {
-        int[] nextSmallerElement = nextSmallerElementIndices(heights, heights.length);
-        int[] prevSmallerElement = previousSmallerElementIndices(heights, heights.length);
+    // Maximum area in a Histogram Problem
+    public int maximumAreaInHistogram(char[] arr){
+        int n = arr.length;
+        int[] nextSmaller = new int[n];
+        int[] prevSmaller = new int[n];
+        
+        prevSmaller[0] = -1;
+        nextSmaller[n-1] = n;
+        
+        for (int i=1; i<n; i++){
+            int low = i-1;         
+            while (low>=0 && arr[low] >= arr[i])
+                low = prevSmaller[low];
+            
+            prevSmaller[i] = low;
+        }
+        
+        for (int i = n-2; i>=0; i--){
+            int high = i+1;
+            while (high<n && arr[i] <= arr[high])
+                high = nextSmaller[high];
+            nextSmaller[i] = high;
+        }
         
         int maxArea = 0;
-        
-        for (int i=0; i<heights.length; i++){
-            int area = (nextSmallerElement[i] - prevSmallerElement[i] -1) * heights[i];
-            maxArea = Math.max(maxArea, area);
+        for (int i=0; i<n; i++){
+            int height = arr[i];
+            maxArea = Math.max(maxArea, (nextSmaller[i] - prevSmaller[i] - 1)*height);
         }
-        
         return maxArea;
     }
-
-    // Similar to the problem of Next Greater element, this is the code for problem of
-    // Next Smaller Element using stack
-    public int[] nextSmallerElementIndices(char[] arr, int n){
-        int[] nextSmallerElement = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        
-        for (int i=0; i<n; i++){
-            while(!stack.isEmpty() && arr[i] < arr[stack.peek()])
-                nextSmallerElement[stack.pop()] = i;
-            stack.push(i);
-        }
-        
-        while(!stack.isEmpty())
-            nextSmallerElement[stack.pop()] = n;
-
-        return nextSmallerElement;
-    }
-
-    // Similar to the problem of Next Greater element, this is the code for problem of
-    // Previous Smaller Element using stack
-    public int[] previousSmallerElementIndices(char[] arr, int n){
-        int[] prevSmallerElement = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        
-        for (int i=n-1; i>=0; i--){
-            while(!stack.isEmpty() && arr[i] < arr[stack.peek()])
-                prevSmallerElement[stack.pop()] = i;
-            stack.push(i);
-        }
-        
-        while(!stack.isEmpty())
-            prevSmallerElement[stack.pop()] = -1;
-
-        return prevSmallerElement;
-    }
-    
-//     // Maximum area in a Histogram Problem
-//     public int maximumAreaInHistogram(char[] arr){
-//         int n = arr.length;
-//         int[] nextSmaller = new int[n];
-//         int[] prevSmaller = new int[n];
-        
-//         prevSmaller[0] = -1;
-//         nextSmaller[n-1] = n;
-        
-//         for (int i=1; i<n; i++){
-//             int low = i-1;         
-//             while (low>=0 && arr[low] >= arr[i])
-//                 low = prevSmaller[low];
-            
-//             prevSmaller[i] = low;
-//         }
-        
-//         for (int i = n-2; i>=0; i--){
-//             int high = i+1;
-//             while (high<n && arr[i] <= arr[high])
-//                 high = nextSmaller[high];
-//             nextSmaller[i] = high;
-//         }
-        
-//         int maxArea = 0;
-//         for (int i=0; i<n; i++){
-//             int height = arr[i];
-//             maxArea = Math.max(maxArea, (nextSmaller[i] - prevSmaller[i] - 1)*height);
-//         }
-//         return maxArea;
-//     }
 }
