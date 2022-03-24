d6c314f5043cf7f4dfa71e0c841862ee3cae0e2c
Khushdev Pandit <khushdev20211@iiitd.ac.in>
2022-03-24 17:55:27 +0530

    first non rep char

--- src/Queues/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.java
+++ src/Queues/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.java
@@ -1,8 +1,10 @@
 package Queues.FirstNonRepeatingCharacter;
+import java.util.HashMap;
 
 // https://www.youtube.com/watch?v=5co5Gvp_-S0
 
 class FirstNonRepeatingCharacter {
+
     // O(n^2)
     public int firstUniqChar_O_NSquare(String s) {
         
@@ -21,4 +23,34 @@ class FirstNonRepeatingCharacter {
         
         return -1;
     }
+
+    // O(n)
+    public int firstUniqChar_2(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+        for (int i=0; i<s.length(); i++){
+            if (map.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+
+    // DP Approach (much faster)
+    public int firstUniqChar_3(String s) {
+        char[] charCount = new char[26];
+        for (int i=0; i<s.length(); i++)
+            charCount[s.charAt(i) - 'a']++;
+
+        for (int i=0; i<s.length(); i++){
+            if (charCount[s.charAt(i) - 'a'] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+
 }
