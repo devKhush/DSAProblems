7d3a9ba35a3784adab96f8859dd67991b55824e3
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-03-24 18:31:25 +0530

    Time: 30 ms (52.20%), Space: 42.4 MB (95.77%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,15 +1,26 @@
 class Solution {
     public int firstUniqChar(String s) {
-        char[] count = new char[26];
+        HashMap<Character, Integer> count = new HashMap<>();
+        Deque<Character> queue = new LinkedList<>();
         
         for (int i=0; i<s.length(); i++){
-            count[s.charAt(i) - 'a']++;
+            char ch = s.charAt(i);
+            count.put(ch, count.getOrDefault(ch, 0)+1);
+            
+            if (count.get(ch) == 1)
+                queue.add(ch);
+            
+            while (!queue.isEmpty()  &&  count.get(queue.peek()) != 1)
+                queue.remove();
+            
+            // if (!queue.isEmpty())
+            //     System.out.print(queue.peek() + " ");
+            // else
+            //     System.out.print(-1 + " ");
+                
         }
-        
-        for (int i=0; i<s.length(); i++){
-            if (count[s.charAt(i) - 'a'] == 1)
-                return i;
-        }
-        return -1;
+            
+        return !queue.isEmpty() ? s.indexOf(queue.peek()) : -1;
+    
     }
 }
