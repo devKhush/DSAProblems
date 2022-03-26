49dddd93bba66c6ada7b84e483e6773583382450
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-03-26 21:34:16 +0530

    Time: 13 ms (92.01%), Space: 80.5 MB (36.92%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,30 +1,32 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> stack = new Stack<>();
-        int maxArea = 0;
         int n = heights.length;
-        
-        int[] nextSmall = new int[n];
-        int[] prevSmall = new int[n];
-        
+        int[] nextSmaller = new int[n];
+        int[] prevSmaller = new int[n];
 
+         // Similar to the problem of Next Greater element, this is the code for problem of
+         // Previous Smaller Element without using stack
         for (int i=0; i<n; i++){
-            int j = i-1;
-            while (j>=0 && heights[j] >= heights[i])
-                j = prevSmall[j];
-            prevSmall[i] = j; 
+            int low = i-1;
+            while (low >= 0 && heights[low] >= heights[i])
+                low = prevSmaller[low];
+            prevSmaller[i] = low;
+        }
+
+         // Similar to the problem of Next Greater element, this is the code for problem of
+         // Next Smaller Element without using stack
+        for (int i = n-1; i >= 0; i--){
+            int high = i+1;
+            while (high < n && heights[i] <= heights[high])
+                high = nextSmaller[high];
+            nextSmaller[i] = high;
         }
         
-        for (int i=n-1; i>=0; i--){
-            int j = i+1;
-            while (j<n && heights[j] >= heights[i])
-                j = nextSmall[j];
-            nextSmall[i] = j; 
+        
+        int maxArea = 0;
+        for (int i=0; i<n; i++){
+            maxArea = Math.max(maxArea, (nextSmaller[i]-prevSmaller[i]-1)*heights[i]);
         }
-        
-        for (int i=0; i<n; i++)
-            maxArea = Math.max(maxArea, (nextSmall[i] - prevSmall[i] - 1)*heights[i]);
-        
         return maxArea;
-    }
+     }
 }
