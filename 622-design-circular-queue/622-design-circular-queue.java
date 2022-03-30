3d67d5b106dbd24ba396a6b02082033b9a9be38b
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-03-30 23:23:50 +0530

    Time: 5 ms (77.46%), Space: 48.1 MB (50.35%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.java
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -1,82 +1,59 @@
-
-class Node{
-    public int data;
-    public Node next;
-    
-    public Node(int data){
-        this.data = data;
-    }
-}
-    
-    
 class MyCircularQueue {
-    public Node head = null;
-    public Node tail = null;
+    public int front;
+    public int rear;
     public int size;
     public int maxSize;
+    private int[] queue;
 
-    public MyCircularQueue(int maxSize) {
-        this.maxSize = maxSize;
-        this.size = 0;
+    public MyCircularQueue(int k) {
+        front = 0;
+        rear = -1;
+        size = 0;
+        maxSize = k;
+        queue = new int[k];
     }
     
     public boolean enQueue(int value) {
-        if (this.isFull())
+        if (isFull())
             return false;
         
-        Node node = new Node(value);
-        
-        if (this.head==null){
-            this.head = node;
-            this.tail = node;
-        }
-        else{
-            this.tail.next = node;
-            this.tail = this.tail.next;
-        }
-        this.tail.next = this.head;
-        this.size++;
+        rear = (rear + 1) % maxSize;
+        queue[rear] = value;
+        size++;
         return true;
     }
     
     public boolean deQueue() {
-        if (this.isEmpty())
+        if (isEmpty())
             return false;
         
-        this.tail.next = null;
-        Node node = this.head;
-        this.head = this.head.next;
-        node.next = null;
-        this.size--;
-        
-        if (!this.isEmpty())
-            this.tail.next = this.head;
-        else
-            this.tail = null;
+        int removed = queue[front];
+        size--;
+        front = (front + 1) % maxSize;
         return true;
     }
     
     public int Front() {
-        if (this.isEmpty())
+        if (isEmpty())
             return -1;
-        return this.head.data;
+        return queue[front];
     }
     
     public int Rear() {
-        if (this.isEmpty())
+        if (isEmpty())
             return -1;
-        return this.tail.data;
+        return queue[rear];
     }
     
     public boolean isEmpty() {
-        return this.size == 0;
+        return size == 0;
     }
     
     public boolean isFull() {
-        return this.size == this.maxSize;
+        return size == maxSize;
+
     }
 }
-
 
 /**
  * Your MyCircularQueue object will be instantiated and called as such:
