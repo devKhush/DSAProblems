22776b8e31e1d34b57859e1bbc35f7fdcb09063a
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-04-08 00:28:49 +0530

    Time: 1 ms (11.19%), Space: 41.4 MB (18.16%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,27 +1,14 @@
 class Solution {
-    private int[] tribonacci;
-    
-    public int tribonacci_func(int n) {
-        if (n==0) {
-            tribonacci[n] = 0;
-            return 0;
-        }       
-        if (n==1 || n==2) {
-            tribonacci[n] = 1;
-            return 1;
-        }
-        else if (tribonacci[n] == 0){
-            tribonacci[n] = tribonacci_func(n-1) +  tribonacci_func(n-2) +  tribonacci_func(n-3);
-            return tribonacci[n];
-        }
-        return tribonacci[n];
-    }
+    private HashMap<Integer, Integer> map = new HashMap<>();
     
     public int tribonacci(int n) {
-        this.tribonacci = new int[n+1]; 
-        
-        tribonacci_func(n);
-        
-        return   tribonacci[n];
+        if (n==0)
+            return 0;
+        if (n==1 || n==2)
+            return 1;
+        else if (map.containsKey(n))
+            return map.get(n);
+        map.put(n, tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3));
+        return map.get(n);
     }
 }
