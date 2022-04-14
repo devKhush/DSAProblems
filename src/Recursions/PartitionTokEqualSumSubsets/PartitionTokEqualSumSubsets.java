260a05e4483624ca45ee5e5484a532ce7487db17
Khushdev Pandit <khushdev20211@iiitd.ac.in>
2022-04-14 18:29:51 +0530

    kth largest element... ...

--- src/Recursions/PartitionTokEqualSumSubsets/PartitionTokEqualSumSubsets.java
+++ src/Recursions/PartitionTokEqualSumSubsets/PartitionTokEqualSumSubsets.java
@@ -1,6 +1,8 @@
 package Recursions.PartitionTokEqualSumSubsets;
 import java.util.Arrays;
 
+// https://www.youtube.com/watch?v=qpgqhp_9d1s&t=45s
+
 class PartitionTokEqualSumSubsets {
     public boolean canPartitionKSubsets(int[] nums, int k) {
         if (k==0 || nums==null)
@@ -18,7 +20,9 @@ class PartitionTokEqualSumSubsets {
         int eachPartitionSum = sum / k;
         
         Arrays.sort(nums);
-        
+
+        // if last element is greater than required sum of partition set, then the partition
+        // sets can't be formed
         if (nums[nums.length-1] > eachPartitionSum)
             return false;
         
@@ -26,49 +30,48 @@ class PartitionTokEqualSumSubsets {
     }
     
     private boolean canPartition(int[] nums, boolean[] visited, int k, int eachPartitionSum, int index, int sumInProgress){
-//        System.out.println("\n********************************************* New  Call ******************************************");
-//        System.out.println(Arrays.toString(nums));
-//        System.out.println(Arrays.toString(visited));
-//        System.out.printf("index = %d, sumInProgress = %d, k = %d \n",index, sumInProgress, k);
-
-        if (k==1)
+        if (k == 1)
             return true;
-        
+
+        // if sub set sum is greater than required partition sum, then set can't be formed
         if (sumInProgress > eachPartitionSum)
             return false;
-        
+
+        // if sub set sum is equal to required partition sum, then set can be formed
+        // so check for remaining k-1 subsets
         if (sumInProgress == eachPartitionSum)
             return canPartition(nums, visited, k-1, eachPartitionSum, 0, 0);
         
         for (int i = index; i < nums.length; i++){
-//            System.out.printf("\ni = %d, sumInProgress = %d \n",i, sumInProgress);
-//            System.out.println(Arrays.toString(visited));
             if (!visited[i]){
+                //assume that with current element, a required partition set with required
+                // sum can be formed and mark it visited
                 visited[i] = true;
 
-//                System.out.printf("\ni = %d, sumInProgress = %d \n",i, sumInProgress);
-//                System.out.println(Arrays.toString(visited));
-
+                // visit each remaining element in the array one by one & checks whether these remaining element
+                // in the array can obtain required partition set sum
                 if (canPartition(nums, visited, k, eachPartitionSum, i + 1, sumInProgress + nums[i]))
                     return true;
 
-//                System.out.println("....................... Returned .......................");
+                // if required partition set sum can't be formed with current element,
+                // then don't use it and mark it unused
                 visited[i] = false;
             }
 
+            // if required partition set sum can't be formed with current element,
+            // then don't use it. Also, we can skip the next element in array if they are
+            // same as previous one, as they can form partition set too
             if (!visited[i]){
                 while (i < nums.length-1  &&  nums[i] == nums[i+1])
                     i++;
             }
         }
+
+        // return false as partition set can't be formed with given combination of elements in array or
+        // with given elements of array
         return false;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {4,3,2,3,5,2,1};
-        int k = 4;
-        System.out.println(new PartitionTokEqualSumSubsets().canPartitionKSubsets(arr, k));
-    }
 }
 
 
---
+++ src/Recursions/PartitionTokEqualSumSubsets/PartitionTokEqualSumSubsets_DryRun.java
@@ -0,0 +1,73 @@
+package Recursions.PartitionTokEqualSumSubsets;
+
+import java.util.Arrays;
+
+public class PartitionTokEqualSumSubsets_DryRun {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        if (k==0 || nums==null)
+            return false;
+        if (k==1)
+            return true;
+
+        int sum = 0;
+        for (int value : nums)
+            sum += value;
+
+        if (sum % k != 0)
+            return false;
+
+        int eachPartitionSum = sum / k;
+
+        Arrays.sort(nums);
+
+        if (nums[nums.length-1] > eachPartitionSum)
+            return false;
+
+        return canPartition(nums, new boolean[nums.length], k, eachPartitionSum, 0, 0);
+    }
+
+    private boolean canPartition(int[] nums, boolean[] visited, int k, int eachPartitionSum, int index, int sumInProgress){
+//        System.out.println("\n********************************************* New  Call ******************************************");
+//        System.out.println(Arrays.toString(nums));
+//        System.out.println(Arrays.toString(visited));
+//        System.out.printf("index = %d, sumInProgress = %d, k = %d \n",index, sumInProgress, k);
+
+        if (k==1)
+            return true;
+
+        if (sumInProgress > eachPartitionSum)
+            return false;
+
+        if (sumInProgress == eachPartitionSum)
+            return canPartition(nums, visited, k-1, eachPartitionSum, 0, 0);
+
+        for (int i = index; i < nums.length; i++){
+//            System.out.printf("\ni = %d, sumInProgress = %d \n",i, sumInProgress);
+//            System.out.println(Arrays.toString(visited));
+            if (!visited[i]){
+                visited[i] = true;
+
+//                System.out.printf("\ni = %d, sumInProgress = %d \n",i, sumInProgress);
+//                System.out.println(Arrays.toString(visited));
+
+                if (canPartition(nums, visited, k, eachPartitionSum, i + 1, sumInProgress + nums[i]))
+                    return true;
+
+//                System.out.println("....................... Returned .......................");
+                visited[i] = false;
+            }
+
+            if (!visited[i]){
+                while (i < nums.length-1  &&  nums[i] == nums[i+1])
+                    i++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,3,5,2,1};
+        int k = 4;
+        System.out.println(new PartitionTokEqualSumSubsets().canPartitionKSubsets(arr, k));
+    }
+}
