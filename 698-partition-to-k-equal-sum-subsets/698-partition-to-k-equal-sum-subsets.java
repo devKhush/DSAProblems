0c909d29abd98d04c79b6e17c861f67b38447c5f
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-04-14 17:57:59 +0530

    Time: 6 ms (91.47%), Space: 40 MB (82.85%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -42,6 +42,11 @@ class Solution {
                 
                 visited[i] = false;
             }
+            
+            if (!visited[i]){
+                while (i < nums.length-1 && nums[i]==nums[i+1])
+                    i++;
+            }
         }
         return false;
     }
