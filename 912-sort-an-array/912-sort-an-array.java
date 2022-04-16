b2da81dc51e42998744b4a77366684fc950bdad3
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-04-17 01:02:44 +0530

    Time: 935 ms (16.51%), Space: 55.2 MB (80.69%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    // Merge sort O(n.logn)
     private void merge(int[] arr, int[] temp, int low, int mid, int high){
         int i=low, j=mid+1, k=low;
         
@@ -26,8 +28,41 @@ class Solution {
         }
     }
     
+    // Quick Sort O(n.logn)
+    private int partition(int[] arr, int low, int high){
+        int pivot = arr[low];
+        int i = low+1;
+        int j = high;
+        
+        while (i <= j){
+            while (i < arr.length  &&  arr[i] <= pivot)
+                i++;
+            while (j > 0  &&  arr[j] > pivot)
+                j--;
+            
+            if (i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        arr[low] = arr[j];
+        arr[j] = pivot;
+        return j;
+    }
+    
+    private void quickSort(int[] arr, int low, int high){
+        if (low < high){
+            int partition = partition(arr, low, high);
+            quickSort(arr, low, partition-1);    
+            quickSort(arr, partition+1, high);
+        }
+    }
+    
     public int[] sortArray(int[] nums) {
-        this.mergeSort(nums, new int[nums.length], 0, nums.length-1);
+        // this.mergeSort(nums, new int[nums.length], 0, nums.length-1);
+        
+        quickSort(nums, 0, nums.length-1);
         return nums;
     }
 }
