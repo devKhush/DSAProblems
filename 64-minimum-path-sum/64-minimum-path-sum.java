c13608c8e57cb6e8845ae6f1992a996eca26ac1c
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-05-28 23:52:35 +0530

    Time: 3 ms (53.60%), Space: 46.1 MB (39.26%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -2,26 +2,24 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
         int[][] dp = new int[m][n];
-        for (int[] row: dp){
-            Arrays.fill(row, -1);
+        
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (i==0 && j==0){
+                    dp[0][0] = grid[0][0];
+                    continue;
+                }
+                
+                int minPathByMovingUp = Integer.MAX_VALUE/2, minPathByMovingLeft = Integer.MAX_VALUE/2;
+                
+                if (i > 0) minPathByMovingUp = dp[i-1][j] + grid[i][j];
+                
+                if (j > 0) minPathByMovingLeft = dp[i][j-1] + grid[i][j];
+                
+                dp[i][j] = Math.min(minPathByMovingUp, minPathByMovingLeft);
+            }
         }
-        return memoizationSolution(m-1, n-1, grid, dp);
-
-    }
-    
-    private int memoizationSolution(int i, int j, int[][] grid, int[][] dp){
-        if (i==0 && j==0)
-            return grid[0][0];
-        if (i<0 || j<0)
-            return Integer.MAX_VALUE/2;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int minPathByMovingUp = memoizationSolution(i-1, j, grid, dp) + grid[i][j];
-        int minPathByMovingLeft = memoizationSolution(i, j-1, grid, dp) + grid[i][j];
-
-        return dp[i][j] = Math.min(minPathByMovingUp, minPathByMovingLeft);
+        return dp[m-1][n-1];
     }
 }
