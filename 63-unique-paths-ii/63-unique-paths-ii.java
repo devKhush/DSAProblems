7488dc081b70a9cdf5eb2e9676e61df0e1be93aa
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-05-28 18:16:55 +0530

    Time: 1 ms (52.92%), Space: 42 MB (66.33%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,5 +1,36 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+         int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        // If obstacle is at the (0,0), then we can't reach from (0,0) to (m-1,n-1) or vice-versa
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+        
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i==0 && j==0)
+                    continue;
+                
+                if (obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                int totalWaysByMovingUp = 0, totalWaysByMovingLeft = 0;
+                if (i > 0) totalWaysByMovingUp = dp[i-1][j];
+                if (j > 0) totalWaysByMovingLeft = dp[i][j-1]; 
+                dp[i][j] = totalWaysByMovingLeft + totalWaysByMovingUp;
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+    
+    public int uniquePathsWithObstacles_Memoization(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         
