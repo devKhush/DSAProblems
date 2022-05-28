11a091a893159d60682d41b5886354d71bc7688f
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-05-28 19:04:34 +0530

    Time: 1 ms (52.92%), Space: 42.5 MB (28.28%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,64 +1,31 @@
 class Solution {
      public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-         int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-
-      
-        
-        dp[0][0] = 1;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                                
-                if (obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
-                    continue;
-                }                
-
-                
-                if (i==0 && j==0)
-                    continue;
-                
-                
-                int totalWaysByMovingUp = 0, totalWaysByMovingLeft = 0;
-                if (i > 0) totalWaysByMovingUp = dp[i-1][j];
-                if (j > 0) totalWaysByMovingLeft = dp[i][j-1]; 
-                dp[i][j] = totalWaysByMovingLeft + totalWaysByMovingUp;
-            }
-        }
-        
-        return dp[m-1][n-1];
-    }
-    
-    public int uniquePathsWithObstacles_Memoization(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        
-        
-        int[][] dp = new int[m][n];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-        
-        return solve(m-1, n-1, dp, obstacleGrid);
+         
+        int[] dp = new int[n];
+        dp[0] = 1;
+         
+        for (int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if (obstacleGrid[i][j] == 1){
+                    dp[j] = 0;
+                    continue;
+                }
+                
+                if (i==0 && j==0) continue;
+                
+                int totalWaysByMovingUp = 0, totalWaysByMovingLeft = 0;
+                
+                if (i > 0) totalWaysByMovingUp = dp[j];
+                if (j > 0) totalWaysByMovingLeft = dp[j-1];
+                
+                dp[j] = totalWaysByMovingLeft + totalWaysByMovingUp; 
+            }
+        }
+         
+      return dp[n-1];
     }
     
-    public int solve(int i, int j, int[][] dp, int[][] arr){
-        if (i<0 || j<0)
-            return 0;
-        
-        if (arr[i][j] == 1)
-            return 0;
-        
-        if (i==0 && j==0)
-            return 1;
-        
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        
-        int totalWaysByMovingUp = solve(i-1, j, dp, arr);      
-        int totalWaysByMovingLeft = solve(i, j-1, dp, arr);
-
-        return dp[i][j] = totalWaysByMovingLeft + totalWaysByMovingUp;
-    }
+ 
 }
