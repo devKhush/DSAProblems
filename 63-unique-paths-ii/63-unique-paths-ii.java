93d4e397eca57b5d2f8380ee061c0ef40a1a4b36
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-05-28 17:45:43 +0530

    Time: 1 ms (52.92%), Space: 42.5 MB (28.28%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+        
+        int[][] dp = new int[m][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        return solve(m-1, n-1, dp, obstacleGrid);
+    }
+    
+    public int solve(int i, int j, int[][] dp, int[][] arr){
+        if (i==0 && j==0)
+            return arr[i][j] != 1 ? 1 : 0;
+        
+        if (i<0 || j<0)
+            return 0;
+        if (arr[i][j] == 1)
+            return 0;
+        
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        
+        int totalWaysByMovingUp = solve(i-1, j, dp, arr);      
+        int totalWaysByMovingLeft = solve(i, j-1, dp, arr);
+
+        return dp[i][j] = totalWaysByMovingLeft + totalWaysByMovingUp;
+    }
+}
