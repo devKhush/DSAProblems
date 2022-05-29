6f93c53fe24025f624d98c3d6b74b6acb5a5c098
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-05-30 00:44:56 +0530

    Time: 4 ms (61.58%), Space: 44.8 MB (15.53%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,41 +1,37 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[][] dp = new int[n][n];
-        for (int[] row: dp)
-            Arrays.fill(row, -1);
+       int n = triangle.size();
+        int[] dp = new int[n];
+        
+        // To calculate the min path from (0,0) to (i,j) we need previous top value (in previous row) 
+        // and previous top-left diagonal value (also in previous row)
+        // One DP array will not work here, as then how will we store the min path of current row 
+        // We use one DP array 'dp' to get these required previous values.
+        // And store the min path of current row values in another dp array 'tempDP'
         
         for (int i = 0; i < n; i++){
+            int[] tempDP = new int[n];
+            
             for (int j = 0; j <= i; j++){
-                // Base case
                 if (i==0 && j==0){
-                    dp[0][0] = triangle.get(0).get(0);
+                    tempDP[0] = triangle.get(0).get(0);
                     continue;
                 }
                 
-                int minPathByMovingUp = 0, minPathByMovingUpLeft = 0;
-
+                int minPathByMovingUp = (j <= i-1) ? dp[j] + triangle.get(i).get(j) : Integer.MAX_VALUE/2;
                 
-                if (j <= i-1)
-                    minPathByMovingUp = dp[i-1][j] + triangle.get(i).get(j);
-                else
-                    minPathByMovingUp = Integer.MAX_VALUE/2;
-
-                if (j > 0)
-                    minPathByMovingUpLeft = dp[i-1][j-1] + triangle.get(i).get(j);
-                else
-                    minPathByMovingUpLeft = Integer.MAX_VALUE/2;
+                int minPathByMovingUpLeft = (j > 0) ? dp[j-1] + triangle.get(i).get(j) : Integer.MAX_VALUE/2;
                 
-                dp[i][j] = Math.min(minPathByMovingUp, minPathByMovingUpLeft);
+                tempDP[j] = Math.min(minPathByMovingUpLeft, minPathByMovingUp);
             }
+            dp = tempDP;
         }
         
-        int minPathFromBottomToTop = Integer.MAX_VALUE;
+        int minPathSum = Integer.MAX_VALUE;
+        for (int pathSum : dp)
+            minPathSum = Math.min(pathSum, minPathSum);
         
-        for (int j = 0; j < n; j++)
-            minPathFromBottomToTop = Math.min(minPathFromBottomToTop, dp[n-1][j]);
-        
-        return minPathFromBottomToTop;
+        return minPathSum;
         
     }
 }
