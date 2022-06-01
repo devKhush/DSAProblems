cb09e6476040384be13a1eb462eb8be290cee251
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-01 17:37:15 +0530

    Time: 57 ms (12.76%), Space: 46.9 MB (36.89%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,44 +1,54 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-           int m = grid.length;
+        int m = grid.length;
         int n = grid[0].length;
 
-        int[][][] dp = new int[m][n][n];
+        // We only store the columns j1 & j2 in DP array & not row numbers
+        int[][] dp = new int[n][n];
 
-        for (int j1 = 0; j1 < n; j1++){
+        // Base cases
+        // if j1==j2 (both person came to same position in last row), then only one person can pick up the cherry from that position
+        // else (both person are at diff. position in last row), then both person can pick up the cherry from their position
+        for (int j1 = 0; j1 < n; j1++) {
             for (int j2 = 0; j2 < n; j2++)
-                dp[m-1][j1][j2] =  j1 == j2 ? grid[m-1][j1] : grid[m-1][j1] + grid[m-1][j2];
+                dp[j1][j2] = j1 == j2 ? grid[m-1][j1] : grid[m-1][j1] + grid[m-1][j2];
         }
 
+        // Other cases
+        for (int i = m-2; i >=0; i--){
+            int[][] tempDP = new int[n][n];
 
-        for (int i = m-2; i >= 0; i--){
-            for (int j1 = 0; j1 < n; j1++){
-                for (int j2 = 0; j2 < n; j2++){
+            for (int j1 = 0; j1 < n; j1++) {
+                for (int j2 = 0; j2 < n; j2++) {
 
-                    int maxCherryPicked = Integer.MIN_VALUE;
-                    
-                    for (int dj1 = -1; dj1 <= 1; dj1++){
-                        for (int dj2 = -1; dj2 <= 1; dj2++) {
-                            
-                            if (j1 + dj1 < 0 || j2 + dj2 < 0 || j1 + dj1 >= n || j2 + dj2 >=n )
+                    int maxCherryPicked = -1;
+
+                    for (int dj1 = -1; dj1 <=1; dj1++){
+                        for (int dj2 = -1; dj2 <= 1; dj2++){
+
+                            boolean isOutOfBoundary = j1 + dj1 < 0 || j2 + dj2 < 0 || j1 + dj1 >=n || j2 + dj2 >= n;
+                            if (isOutOfBoundary)
                                 continue;
-                            
+
                             int currCherryPicked;
-                            
                             if (j1 == j2)
-                                currCherryPicked = grid[i][j1] + dp[i+1][j1 + dj1][j2 + dj2];
+                                currCherryPicked = grid[i][j1] + dp[j1 + dj1][j2 + dj2];
                             else
-                                currCherryPicked = grid[i][j1] + grid[i][j2] + dp[i+1][j1 + dj1][j2 + dj2];
-                            
+                                currCherryPicked = grid[i][j1] + grid[i][j2] + dp[j1 + dj1][j2 + dj2];
+
                             maxCherryPicked = Math.max(maxCherryPicked, currCherryPicked);
                         }
                     }
-                    
-                    dp[i][j1][j2] = maxCherryPicked;
+
+                    tempDP[j1][j2] = maxCherryPicked;
                 }
             }
+            dp = tempDP;
         }
-        return dp[0][0][n-1];
+
+        // Both person starts from here, one from (0,0) & other from (0,n-1)
+        // So, answer will be stored in dp[0][n-1]
+        return dp[0][n-1];
     }
     
     
