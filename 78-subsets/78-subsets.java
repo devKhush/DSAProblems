47fa08671f24ad203ce9de992087a93a9d51f470
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-02 23:57:23 +0530

    Time: 1 ms (80.26%), Space: 43.2 MB (59.44%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,5 +1,28 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+    // ******************************* Approach 1 ******************************* 
+
+    public List<List<Integer>> subsets(int[] arr) {
+        List<List<Integer>> answer = new ArrayList<>();
+
+        getAllSubsequences(0, arr, answer, new ArrayList<>());
+        return answer;
+    }
+    
+    private void getAllSubsequences(int index, int[] arr, List<List<Integer>> allSubSequence, ArrayList<Integer> list){
+        allSubSequence.add(new ArrayList<>(list));
+
+        for (int i = index; i < arr.length; i++){
+
+            list.add(arr[i]);
+            getAllSubsequences(i+1, arr, allSubSequence, list);
+            list.remove(list.size() - 1);
+
+        }
+    }
+    
+    // ******************************* Approach 2 ******************************* 
+    
+    public List<List<Integer>> subsets_V1(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
         
         getSubSequences(0, nums, new ArrayList<>(), answer);
