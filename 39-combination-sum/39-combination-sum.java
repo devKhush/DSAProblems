3642667426503b47d15838e4b761b3bafa5b636f
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-02 16:27:29 +0530

    Time: 5 ms (61.87%), Space: 45 MB (39.95%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -3,8 +3,8 @@ class Solution {
     private List<List<Integer>> allCombinations = new ArrayList<>();
     
     private void getAllCombinationSums(int index, int target, int currentSum, int[] arr, ArrayList<Integer> list){
-        if (currentSum > target)
-            return;
+        // if (currentSum > target)
+        //     return;
         
         if (index == arr.length){
             if (currentSum == target)
@@ -12,14 +12,14 @@ class Solution {
             return;
         }
         
-                
+        if (target - currentSum >= arr[index]){
         // Picking the current element again
         currentSum += arr[index];
         list.add(arr[index]);
         getAllCombinationSums(index, target, currentSum, arr, list);
-        list.remove((Object) arr[index]);
+        list.remove(list.size() - 1);
         currentSum -= arr[index];
-        
+        }
         // Not Picking the current element
         getAllCombinationSums(index + 1, target, currentSum, arr, list);
 
