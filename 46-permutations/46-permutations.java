bfd7afc01457d1a7b58414e09dba4e8f756bd363
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-04 00:32:22 +0530

    Time: 1 ms (95.87%), Space: 44.7 MB (49.47%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,29 +1,36 @@
 class Solution {
+    
+     // ************************************* Approach 2 *************************************************
+ 
     public List<List<Integer>> permute(int[] arr) {
         List<List<Integer>> allPermutations = new ArrayList<>();
         
-        allPermutations(arr, 0, new boolean[arr.length], new ArrayList<>(), allPermutations);
+        allPermutations(0, arr, allPermutations);
         
         return allPermutations;
+        
     }
     
-    private void allPermutations(int[] arr, int elementPicked, boolean[] visited, ArrayList<Integer> list, List<List<Integer>> answer){
-        if (elementPicked == arr.length){
-            answer.add(new ArrayList<>(list));
-            return;
+    private void allPermutations(int index, int[] arr, List<List<Integer>> answer){
+        if (index == arr.length){
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int val : arr)
+                list.add(val);
+            
+            answer.add(list);
         }
         
-        for (int i = 0; i < arr.length; i++){
-            if (!visited[i]){
-                
-                visited[i] = true;
-                list.add(arr[i]);
-                
-                allPermutations(arr, elementPicked + 1, visited, list, answer);
-
-                list.remove(list.size() - 1);
-                visited[i] = false;
-            }
+        
+        for (int i = index; i < arr.length; i++){
+                swap(index, i, arr);
+                allPermutations(index + 1, arr, answer);
+                swap(index, i, arr);
         }
+    }
+    
+    private void swap(int i, int j, int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
