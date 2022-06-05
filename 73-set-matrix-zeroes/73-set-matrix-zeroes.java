6835a030d321102cfe8fcd1961ba8723c03da5c5
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-05 15:23:42 +0530

    Time: 1 ms (93.39%), Space: 53.4 MB (68.04%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,68 +1,41 @@
 class Solution { 
-     public void setZeroes(int[][] matrix) {
+    
+    public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
 
-
-       for (int i = 0; i < m; i++){
-            for (int j  = 0; j < n; j++){
-                if (matrix[i][j] == 0){
-
-                    int row = i -1;
-                    while (row >= 0){
-                            matrix[row][j] = -999;
-                        row--;
-                    }
-
-                    row = i + 1;
-                    while (row < m){
-                        if (matrix[row][j] != 0)
-                            matrix[row][j] = -999;
-                        row++;
-                    }
-
-                    int column = j -1;
-                    while (column >= 0){
-                            matrix[i][column] = -999;
-                        column--;
-                    }
-
-                    column = j + 1;
-                    while (column < n){
-                        if (matrix[i][column] != 0)
-                            matrix[i][column] = -999;
-                        column++;
-                    }
-                }
+        boolean row0_HasToBeZero = false;
+        boolean column0_HasToBeZero = false;
+        
+        for (int i = 0; i < m; i++)
+            if (matrix[i][0] == 0)
+                column0_HasToBeZero = true;
+        
+        for (int j = 0; j < n; j++)
+            if (matrix[0][j] == 0)
+                row0_HasToBeZero = true;
+        
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++)
+               if (matrix[i][j] == 0)
+                   matrix[0][j] = matrix[i][0] = 0;
+        }
+            
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
             }
         }
         
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == -999)
-                    matrix[i][j] = 0;
-            }
+        if (row0_HasToBeZero){
+            for (int j = 0; j < n; j++)
+                matrix[0][j] = 0;
         }
-    }
-    
-    
-    public void setZeroes_(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-       for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-
-                if (matrix[i][j] == 0)
-                    matrix[i][0] = matrix[0][j] = 0;
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[0][j] == 0 || matrix[i][0] == 0)
-                    matrix[i][j] = 0;
-            }
-        }
+        
+        if (column0_HasToBeZero)
+            for (int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        
     }
 }
