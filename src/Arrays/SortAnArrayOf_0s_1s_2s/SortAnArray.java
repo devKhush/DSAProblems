357c696aee9f650eaacaaf71576110c6bbcee203
devKhush999 <khushdev7838@gmail.com>
2022-06-06 01:05:30 +0530

    Some changes done...

--- src/Arrays/SortAnArrayOf_0s_1s_2s/SortAnArray.java
+++ src/Arrays/SortAnArrayOf_0s_1s_2s/SortAnArray.java
@@ -1,5 +1,7 @@
 package Arrays.SortAnArrayOf_0s_1s_2s;// { Driver Code Starts
 
+// https://takeuforward.org/data-structure/sort-an-array-of-0s-1s-and-2s/
+
 class SortAnArray{
 
     public static void sort012(int a[], int n){
@@ -15,19 +17,16 @@ class SortAnArray{
 
         int i = 0;
 
-        while (i < count0)
-            a[i++] = 0;
+        while (i < count0)  a[i++] = 0;
 
-        while (i < count0  + count1)
-            a[i++] = 1;
+        while (i < count0  + count1)    a[i++] = 1;
 
         // We don't need a variable that stores count of 2, as array contains
         // only 0s, 1s & 2s (counts of 0s & 1s are already stored)
         // while (i < count0  + count1  + count2)
         //     a[i++] = 2;
 
-        while (i < n)
-            a[i++] = 2;
+        while (i < n)   a[i++] = 2;
 
     }
 }
