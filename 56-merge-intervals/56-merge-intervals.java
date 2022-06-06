c29ea7414ef4eb5af5640aacde27d2a935d0f581
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-06 22:52:34 +0530

    Time: 14 ms (42.18%), Space: 55.7 MB (11.82%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -30,18 +30,39 @@ class Solution {
 
 
 
+
 class IntervalComparator implements Comparator<int[]>{
+
     @Override
     public int compare(int[] a, int[] b) {
-        if (a[0] > b[0])
+        // Sorting according to the Starting time of various Intervals
+        
+        // Case for a[0] > b[0]
+        if (a[0] > b[0]) 
             return 1;
-        else if (a[0] < b[0])
-            return -1;
-        else
-            return 0;
 
+        // Case for a[0] < b[0]
+        else if (a[0] < b[0]) {
+            return -1;
+        }
+
+        // Case for a[0] == b[0]
+        else {
+            // Sorting according to the Ending time of various Intervals in case their starting time is same
+            // Case for  a[1] > b[1]
+            if (a[1] > b[1])
+                return 1;
+            
+            // Case for  a[1] < b[1]
+            else if (a[1] < b[1])
+                return -1;
+
+            // Case for  a[1] == b[1]
+            else
+                return 0;
+        }
+        
         // Shorthand for above code
         // return Integer.compare(a[0], b[0]);
     }
 }
-
