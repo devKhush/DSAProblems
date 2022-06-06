5511039f993cc566db09ceffbc8058e92b59a5c0
devKhush999 <khushdev7838@gmail.com>
2022-06-06 23:11:15 +0530

    Merge Intervals in progress

--- src/MergeIntervals/MergeIntervals_BestApproach.java
+++ src/MergeIntervals/MergeIntervals_BestApproach.java
@@ -1,7 +1,19 @@
-class Solution {
+package MergeIntervals;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MergeIntervals_BestApproach {
+
     public int[][] merge(int[][] intervals) {
+
+        // Sorting intervals according to their starting time
         Arrays.sort(intervals, new IntervalComparator());
 
+        // Another way of sorting interval according to the starting time
+        // Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+
+        // This is ArrayList of required non-overlapping intervals that cover all the intervals in the input
         ArrayList<int[]> arrangedInterval = new ArrayList<>();
         int[] currentInterval = intervals[0];
 
@@ -9,21 +21,25 @@ class Solution {
 
         for (int[] nextInterval : intervals){
 
+            // If the ending time of current interval is less than the starting time of next interval
+            // then we don't need to change current interval, we can move to next interval
             if (currentInterval[1] < nextInterval[0]){
                 currentInterval = nextInterval;
                 arrangedInterval.add(currentInterval);
             }
 
+            // If the ending time of current interval is >= than the starting time of next interval
+            // then the both the current intervals are overlapping & are running in parallel
+            // so we take the maximum ending time of both these intervals & updates it into the current interval
             else if (currentInterval[1] >= nextInterval[0])
                 currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
-
         }
 
         // We can return a list of arranged intervals if required
         int[][] arrangedIntervalArray = new int[arrangedInterval.size()][2];
         for (int i = 0; i < arrangedIntervalArray.length; i++)
             arrangedIntervalArray[i] = arrangedInterval.get(i);
-        
+
         return arrangedIntervalArray;
     }
 }
@@ -36,12 +52,12 @@ class IntervalComparator implements Comparator<int[]>{
     @Override
     public int compare(int[] a, int[] b) {
         // Sorting according to the Starting time of various Intervals
-        
+
         // Case for a[0] > b[0]
-        if (a[0] > b[0]) 
+        if (a[0] > b[0])
             return 1;
 
-        // Case for a[0] < b[0]
+            // Case for a[0] < b[0]
         else if (a[0] < b[0]) {
             return -1;
         }
@@ -52,16 +68,16 @@ class IntervalComparator implements Comparator<int[]>{
             // Case for  a[1] > b[1]
             if (a[1] > b[1])
                 return 1;
-            
-            // Case for  a[1] < b[1]
+
+                // Case for  a[1] < b[1]
             else if (a[1] < b[1])
                 return -1;
 
-            // Case for  a[1] == b[1]
+                // Case for  a[1] == b[1]
             else
                 return 0;
         }
-        
+
         // Shorthand for above code
         // return Integer.compare(a[0], b[0]);
     }
