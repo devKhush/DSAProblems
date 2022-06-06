3ff5ca4b9e33de8778fead1d04d5ca0d5e9ee7f8
devKhush999 <khushdev7838@gmail.com>
2022-06-06 12:54:48 +0530

    RotateImage

--- src/Arrays/RotateImage/RotateImage.java
+++ src/Arrays/RotateImage/RotateImage.java
@@ -1,15 +1,24 @@
-class Solution {
+package Arrays.RotateImage;
+
+class RotateImage {
+
+    // ************************************ Brute Force ************************************
+    // Make a new matrix & store the mirror image in it & then copy it into original matrix
+    // T.C. --> O(n*n)
+    // S.C. --> O(n*n)
+
     public void rotate(int[][] matrix) {
         int n =  matrix.length;
-        
+
         int[][] temp = new int[n][n];
-        
+
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 temp[i][j] =  matrix[n -1 -j][i];
-        
+
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 matrix[i][j] = temp[i][j];
     }
+
 }
