4ed3e1ba2001c1a17cd7e3d74ffb69a33a93afdf
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-07 22:35:40 +0530

    Time: 10 ms (34.29%), Space: 45.5 MB (96.22%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -2,16 +2,15 @@ class Solution {
     
     public List<Integer> majorityElement(int[] arr) {
         int n = arr.length;
-        
         HashMap<Integer, Integer> count = new HashMap<>();
         List<Integer> majorityElements = new ArrayList<>();
-        
-        for (int num : arr){
+
+        for (int num : arr)
             count.put(num, count.getOrDefault(num, 0) + 1);
-            
-            if (count.get(num) > n/3  &&  !majorityElements.contains(num))
+
+        for (int num : count.keySet())
+            if (count.get(num) > n/3)
                 majorityElements.add(num);
-        }
         
         return majorityElements;
     }
