a7249b252a2279f59105d958c0422952c007dfd5
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-07 16:54:59 +0530

    Time: 1 ms (42.55%), Space: 40.5 MB (68.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,5 +1,27 @@
 class Solution {
+    
     public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (i == 0 && j == 0) continue;
+                
+                int moveUp = 0, moveLeft = 0;
+                
+                if (i > 0) moveUp = dp[i-1][j];
+                if (j > 0) moveLeft = dp[i][j-1];
+                
+                dp[i][j] = moveUp + moveLeft;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    
+    
+    // Memoization ************************************************************************
+    public int uniquePaths_memo(int m, int n) {
         int[][] dp = new int[m][n];
 
         for (int[] row : dp)
