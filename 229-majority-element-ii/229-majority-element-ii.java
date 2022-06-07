3b5983ac291083903eba306326edc4313fe60c87
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-07 22:13:30 +0530

    Time: 12 ms (27.75%), Space: 51.3 MB (12.94%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,5 +1,23 @@
 class Solution {
+    
     public List<Integer> majorityElement(int[] arr) {
+        int n = arr.length;
+        
+        HashMap<Integer, Integer> count = new HashMap<>();
+        List<Integer> majorityElements = new ArrayList<>();
+        
+        for (int num : arr){
+            count.put(num, count.getOrDefault(num, 0) + 1);
+            
+            if (count.get(num) > n/3  &&  !majorityElements.contains(num))
+                majorityElements.add(num);
+        }
+        
+        return majorityElements;
+    }
+    
+    // Brute force *********************************************************************
+    public List<Integer> majorityElement_BruteForce(int[] arr) {
         int n = arr.length;
         
         List<Integer> majorityElements = new ArrayList<>();
