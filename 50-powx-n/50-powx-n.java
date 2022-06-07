8420ac6290b6852e42bd87590f6bac44d80ff1a4
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-07 15:34:21 +0530

    Time: 1 ms (77.28%), Space: 41.2 MB (95.60%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,18 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0)
-            return 1;
-        
-        double halfPower = myPow(x, n/2);
-        
-        if (n % 2 == 0)
-            return halfPower * halfPower;
-        else{
-            if (n > 0)
-                return halfPower * halfPower * x;
-            else
-                return halfPower * halfPower / x;
+        long N = Math.abs((long)n);
+        double power = 1; 
+
+        while (N != 0){
+            
+            if (N % 2 == 0){
+                x = x * x;
+                N /= 2;
+            }
+            else{
+                power = power * x;
+                N--;
+            }
         }
+        
+        return n >= 0 ? power : 1/power;
         
     }
 }
