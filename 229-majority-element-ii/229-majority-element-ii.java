b89a46bf18c238226e9a0168fe1bfd1ddbe308ec
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-07 23:47:27 +0530

    Time: 3 ms (75.56%), Space: 49.4 MB (60.42%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -2,6 +2,58 @@ class Solution {
     
     public List<Integer> majorityElement(int[] arr) {
         int n = arr.length;
+        int majorElement1 = 0, count1 = 0;
+        int majorElement2 = 0, count2 = 0;
+        
+        for (int num : arr){
+            if (num == majorElement1)
+                count1++;
+            
+            else if (num == majorElement2)
+                count2++;
+            
+            else if (count1 == 0){
+                majorElement1 = num;
+                count1 = 1;
+            }
+            
+            else if (count2 == 0){
+                majorElement2 = num;
+                count2 = 1;
+            }
+            
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        int majorElement1_Count = 0;
+        int majorElement2_Count = 0;
+        for (int num : arr){
+            if (num == majorElement1)
+                majorElement1_Count++;
+            
+            else if (num == majorElement2)
+                majorElement2_Count++;
+        }
+        
+        List<Integer> allMajorElements = new ArrayList<>();
+        
+        if (majorElement1_Count > n/3)
+            allMajorElements.add(majorElement1);
+        
+        if (majorElement2_Count > n/3)
+            allMajorElements.add(majorElement2);
+        
+        return allMajorElements;
+    }
+    
+    
+    
+    // HashMap Solution ***************************************************************
+    public List<Integer> majorityElement_HashMap(int[] arr) {
+        int n = arr.length;
         HashMap<Integer, Integer> count = new HashMap<>();
         List<Integer> majorityElements = new ArrayList<>();
 
@@ -14,6 +66,7 @@ class Solution {
         
         return majorityElements;
     }
+    
     
     // Brute force *********************************************************************
     public List<Integer> majorityElement_BruteForce(int[] arr) {
