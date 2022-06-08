c3b8dc69c101ea0705b91153eb9193c17ea673ad
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-08 13:02:46 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (33.82%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,9 +1,37 @@
 class Solution {
-    private boolean binarySearchMatrix(int low, int high, int target, int[][] matrix){
-        return true;
+    private boolean binarySearchMatrix(int low, int high, int target, int[][] matrix, int totalRow, int totalColumn){
+        if (low > high)
+            return false;
+        
+        int mid = (low + high)/2;
+        int row = mid / totalColumn;
+        int column = mid % totalColumn;
+        // System.out.println(low + " "+high + "    " + mid + " "+ row + " "+ column);
+        
+        if (row >= totalRow || column >= totalColumn)
+            return false;
+        
+        if (matrix[row][column] == target)
+            return true;
+        
+        else if (matrix[row][column] > target)
+            return binarySearchMatrix(low, mid - 1, target, matrix, totalRow, totalColumn);
+        
+        // matrix[row][column] < target
+        else
+            return binarySearchMatrix(mid + 1, high, target, matrix, totalRow, totalColumn);
     }
     
     public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        return binarySearchMatrix(0, m*n -1, target, matrix, m, n);
+    }
+    
+    
+    // Brute force : Linear search ***********************************************************
+    public boolean searchMatrix_BruteForce(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
                  
