12570031776564e59ce6418c849b21221e6d907a
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-08 13:09:07 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (94.67%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,24 +1,49 @@
 class Solution {
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int low = 0, high = m*n - 1;
+        
+        while (low <= high){
+            int mid = (low + high)/2;
+            
+            int row = mid / n;
+            int column = mid % n;
+            
+            if (matrix[row][column] == target)
+                return true;
+            
+            else if (matrix[row][column] > target)
+                high = mid -1;
+            
+            else if (matrix[row][column] < target)
+                low = mid + 1;    
+        }
+        
+        return false;
+    }
+    
+    
+    // Binary Search Recusrive **************************************************************
     private boolean binarySearchMatrix(int low, int high, int target, int[][] matrix, int totalRow, int totalColumn){
         if (low > high)
             return false;
-        
+    
         int mid = (low + high)/2;
         int row = mid / totalColumn;
         int column = mid % totalColumn;
         
         if (matrix[row][column] == target)
             return true;
-        
         else if (matrix[row][column] > target)
-            return binarySearchMatrix(low, mid - 1, target, matrix, totalRow, totalColumn);
-        
-        // matrix[row][column] < target
+            return binarySearchMatrix(low, mid - 1, target, matrix, totalRow, totalColumn);        
         else
             return binarySearchMatrix(mid + 1, high, target, matrix, totalRow, totalColumn);
     }
     
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix_Recursive(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
 
