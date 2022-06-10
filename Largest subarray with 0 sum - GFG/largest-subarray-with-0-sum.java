e5b625b2904a44d7138158d561eadd5aadda8758
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-10 16:20:19 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
@@ -28,26 +28,28 @@ class MaxLenZeroSumSub
 
 class GfG{
     int maxLen(int arr[], int n){
-        int maxSubArrayLength = 0;
+        int maxSubArrayLength = 0, k = 0;
         int prefixSum = 0;
-        
+
         HashMap<Integer, Integer> prefixSumMap = new HashMap<>();
 
         for (int i = 0; i < arr.length; i++){
             prefixSum += arr[i];
 
-            // we don't meed Math.max() condition here, as if the entire prefix sum becomes 0 at any index 'i'
-            // then it will be of maximum length till index 'i' (think)
-            if (prefixSum == 0)
+            // we don't meed Math.max() condition here, as if the entire prefix sum becomes 'k' at any index 'i'
+            // then it will be of maximum length till index 'i' (think...)
+            if (prefixSum == k)
                 maxSubArrayLength = i + 1;
 
-
-            else if (prefixSumMap.containsKey(prefixSum)) {
-                int startIndexOfSum = prefixSumMap.get(prefixSum);
+            else if (prefixSumMap.containsKey(prefixSum - k)) {
+                int startIndexOfSum = prefixSumMap.get(prefixSum - k);
                 int endingIndexOfSameSum = i;
                 maxSubArrayLength = Math.max(maxSubArrayLength, endingIndexOfSameSum - startIndexOfSum);
             }
-            
+
+            // We do not update the 'prefixSum' in HashMap if 'prefixSum' exits earlier in the map
+            // because we want maximum Sub array length with sum 0
+            // So we added a 'else block' here
             else
                 prefixSumMap.put(prefixSum, i);
         }
