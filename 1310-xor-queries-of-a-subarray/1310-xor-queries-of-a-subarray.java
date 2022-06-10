cf10f32f85e231978dd340883b8e809a6bccb3df
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-10 22:16:54 +0530

    Time: 1 ms (100.00%), Space: 54.2 MB (90.65%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -1,7 +1,32 @@
 class Solution {
     
-    // Efficient Hashing Solution **********************************************************
     public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] allXORs = new int[queries.length];
+        int[] prefixXOR = new int[arr.length];
+        
+        int xor = 0;
+        
+        for (int i = 0; i < arr.length; i++){
+            xor = xor ^ arr[i];    
+            prefixXOR[i] = xor;
+        }
+        
+        for (int i = 0; i < queries.length; i++){
+            int queryStartIndex = queries[i][0];
+            int queryEndIndex = queries[i][1];
+            
+            int startXor = queryStartIndex - 1 >= 0 ? prefixXOR[queryStartIndex - 1] : 0;
+            int endXor = prefixXOR[queryEndIndex];
+            
+            allXORs[i] = endXor ^ startXor;
+        }
+        
+        return allXORs;
+    }
+    
+    
+    // Efficient Hashing Solution **********************************************************
+    public int[] xorQueries_HashMap(int[] arr, int[][] queries) {
         int [] allXORs = new int[queries.length];
         
         HashMap<Integer, Integer> prefixXORMap = new HashMap<>();
