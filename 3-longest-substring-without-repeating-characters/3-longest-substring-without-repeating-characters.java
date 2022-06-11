bf1cdc1700aca252f2b50866b2211f4b5359899e
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-11 17:19:10 +0530

    Time: 15 ms (35.94%), Space: 44.4 MB (60.87%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,5 +1,27 @@
 class Solution {
+    
     public int lengthOfLongestSubstring(String str) {
+        int left = 0, right = 0;
+        int maxLengthOfSubstringWithoutRepeatingChars = 0;
+
+        HashMap<Character, Integer> lastSeenAtIndexMap = new HashMap<>();
+        
+        while (right < str.length()){
+            
+            if (lastSeenAtIndexMap.containsKey(str.charAt(right)))
+                left = Math.max(lastSeenAtIndexMap.get(str.charAt(right)) + 1, left);
+            
+            lastSeenAtIndexMap.put(str.charAt(right), right);
+            maxLengthOfSubstringWithoutRepeatingChars = Math.max(right - left + 1, maxLengthOfSubstringWithoutRepeatingChars);
+            right++;
+        }
+        
+        return maxLengthOfSubstringWithoutRepeatingChars;
+    }
+    
+    
+    // Hash Set Solution **********************************************************************
+    public int lengthOfLongestSubstring_Hashing(String str) {
         int left = 0, right = 0;
         int maxLengthOfSubstringWithoutRepeatingChars = 0;
 
