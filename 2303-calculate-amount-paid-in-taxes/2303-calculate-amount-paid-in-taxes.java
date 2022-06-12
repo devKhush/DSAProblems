bb3beeb1510ff92b8c0143051eb9b55b2ff11e45
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-12 13:41:29 +0530

    Time: 1 ms (88.89%), Space: 45.5 MB (11.11%) - LeetHub

---
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double calculateTax(int[][] brackets, int income) {
+        double tax = 0.0;
+
+        int incomeBound = brackets[0][0];
+        int nextIncome = Math.min(incomeBound, income);
+        tax += nextIncome * brackets[0][1] /100.00;
+        income -= nextIncome;
+
+        for (int i = 1; i < brackets.length; i++){
+            incomeBound = brackets[i][0] - brackets[i-1][0];
+            nextIncome = Math.min(incomeBound, income);
+
+            tax += nextIncome * brackets[i][1] /100.0;
+            income -= nextIncome;
+        }
+        return tax;
+    }
+}
