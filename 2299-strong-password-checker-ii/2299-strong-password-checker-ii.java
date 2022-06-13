323c6a9195168df1059dfe0d624e709620c0b188
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-13 19:13:27 +0530

    Time: 1 ms (88.64%), Space: 41.1 MB (88.64%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean strongPasswordCheckerII(String password) {
+        boolean lengthGreaterThan8 = password.length() >= 8;
+        boolean hasLowercase = false;
+        boolean hasUppercase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialCharacter = false;
+        boolean doesNotContainAdjacent = true;
+
+        String specialCharacter  = "!@#$%^&*()-+";
+
+        for (int i = 0; i < password.length(); i++){
+            char ch = password.charAt(i);
+
+            if (i != 0  && ch == password.charAt(i-1))
+                doesNotContainAdjacent = false;
+
+            if (specialCharacter.indexOf(ch) != -1)
+                hasSpecialCharacter = true;
+
+            if (ch >= 'a'  &&  ch <= 'z')
+                hasLowercase = true;
+
+            if (ch >= 'A'  && ch <= 'Z')
+                hasUppercase = true;
+
+            if (ch >= '0'  &&  ch <= '9')
+                hasDigit = true;
+        }
+        return lengthGreaterThan8 && hasDigit && hasLowercase && hasSpecialCharacter && hasUppercase && doesNotContainAdjacent;
+    }
+}
