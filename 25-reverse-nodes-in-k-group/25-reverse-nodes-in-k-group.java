bfa1fa3f53ad11b038871f1b213aa6d72312605a
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-13 20:24:48 +0530

    Time: 1 ms (53.70%), Space: 45.8 MB (41.46%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -9,20 +9,58 @@
  * }
  */
 class Solution {
-    private ListNode reverse(ListNode head){
-        if (head == null || head.next == null)
-           return head;
-        
-        ListNode nextToHead = head.next;
-        head.next = null;
-        
-        ListNode reversedHead = reverse(nextToHead);
-        
-        nextToHead.next = head;
-        return reversedHead;
+    
+    private int getLengthOfList(ListNode head){
+        int length = 0;
+        while (head != null){
+            head = head.next;
+            length++;
+        }
+        return length;
     }
     
     public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode prev = dummy, curr, next;
+        
+        int length = getLengthOfList(head);
+        
+        while (length >= k){
+            curr = prev.next;
+            next = curr.next;
+            
+            for (int i = 1; i < k; i++){
+                curr.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+                next = curr.next;
+            }
+            prev = curr;
+            
+            length -= k;
+        }
+        
+        return dummy.next;
+        
+    }
+    
+    
+    // *********** Approach 1 Dividing list into k groups **********************
+    private ListNode reverse(ListNode head){
+        ListNode curr = head, prev = null, next;
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    
+    public ListNode reverseKGroup_SimpleApproach(ListNode head, int k) {
         ArrayList<ListNode> kthDividedNodes = new ArrayList<>();
         
         ListNode ptr = head, prevNode = null;
