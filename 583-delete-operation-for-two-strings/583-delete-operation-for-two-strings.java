0133dbbe68d1eacea5aec32d10b334ca1783adee
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-14 16:16:57 +0530

    Time: 12 ms (71.63%), Space: 47 MB (48.59%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDistance(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        
+        int len_lcs = LCS(str1, str2, m, n);
+        
+        return m + n - 2*len_lcs;
+    }
+    
+    public int LCS(String str1, String str2, int m, int n){
+        char[] s1 = str1.toCharArray();
+        char[] s2 = str2.toCharArray();
+        
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                
+                if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+    }
+}
