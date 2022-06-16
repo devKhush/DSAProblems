86847a7083edeb7b713ec627b61872829e60b006
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-16 16:21:36 +0530

    Time: 14 ms (97.49%), Space: 43.1 MB (64.12%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,5 +1,40 @@
 class Solution {
-    public String longestPalindrome(String str) {
+    public String longestPalindrome(String string) {
+        int n = string.length();
+        char[] s = string.toCharArray();
+
+        int longestPalindromeLength = 0;
+        int palindromeStartIndex = 0, palindromeEndIndex = 0;
+
+        for (int i = 0; i < s.length; i++){
+            int len1 = expandFromMiddle(s, i, i);
+            int len2 = expandFromMiddle(s, i, i + 1);
+            int currPalindromeExpandedLength = Math.max(len1, len2);
+            
+            if (currPalindromeExpandedLength > longestPalindromeLength){
+                longestPalindromeLength = currPalindromeExpandedLength;
+                palindromeStartIndex = i - (currPalindromeExpandedLength - 1)/2;
+                palindromeEndIndex = i + currPalindromeExpandedLength/2;
+            }
+        }
+        return string.substring(palindromeStartIndex, palindromeEndIndex + 1);
+    }
+
+    
+    private int expandFromMiddle(char[] str, int index1, int index2){
+        int left = index1, right = index2;
+
+        while (left >= 0  &&  right < str.length  &&  str[left] == str[right]){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+    
+    
+    
+    // DP Solution ******************************************************************************
+    public String longestPalindrome_DP(String str) {
         int n = str.length();
         int maxPalindromeLength = 0;
         int palindromeStartIndex = 0, palindromeEndIndex = 0;
