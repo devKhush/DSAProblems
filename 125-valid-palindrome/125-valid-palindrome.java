24326025f8870d84ee1fd90137a3197b71882ebd
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-16 13:42:49 +0530

    Time: 3 ms (98.29%), Space: 42.2 MB (95.14%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,31 +1,26 @@
 class Solution {
-   public boolean isPalindrome(String s) {
-        String str = "";
+   public boolean isPalindrome(String string) {
+        StringBuilder str = new StringBuilder();
+        char[] s = string.toCharArray();
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        for (int i = 0; i < s.length; i++) {
+            char ch = s[i];
             if ((ch >='a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
-                str += ch;
+                str.append(ch);
             if (ch >= 'A' && ch <= 'Z')
-                str += (char)(ch - 'A' + 'a');
+                str.append((char) (ch - 'A' + 'a'));
         }
-        System.out.println(str);
-       
-        int low = 0;
-        int high = str.length() - 1;
+
+        return isPalindrome_Iterative(str.toString(), 0, str.length() - 1);
+    }
+    
+   private boolean isPalindrome_Iterative(String str, int low, int high){
         while (low <= high){
             if (str.charAt(low) != str.charAt(high))
                 return false;
             low++;
             high--;
         }
-       return true;
-        // return isPalindrome_Recursive(str, 0, str.length() - 1);
+        return true;
     }
-    
-    private boolean isPalindrome_Recursive(String s, int low, int high){
-        if (low > high  || low == high)
-            return true;
-        return s.charAt(low) == s.charAt(high)  &&  isPalindrome_Recursive(s, low+1, high-1);
-    } 
 }
