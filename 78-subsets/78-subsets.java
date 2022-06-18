73b77635041a52624ba0484bc5a08e9e65960c0c
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-18 23:53:54 +0530

    Time: 1 ms (86.38%), Space: 43.5 MB (40.50%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,45 +1,22 @@
 class Solution {
-    // ******************************* Approach 1 ******************************* 
-
-    public List<List<Integer>> subsets_V1(int[] arr) {
-        List<List<Integer>> answer = new ArrayList<>();
-
-        getAllSubsequences(0, arr, answer, new ArrayList<>());
-        return answer;
-    }
-    
-    private void getAllSubsequences(int index, int[] arr, List<List<Integer>> allSubSequence, ArrayList<Integer> list){
-        allSubSequence.add(new ArrayList<>(list));
-
-        for (int i = index; i < arr.length; i++){
-
-            list.add(arr[i]);
-            getAllSubsequences(i+1, arr, allSubSequence, list);
-            list.remove(list.size() - 1);
-
-        }
-    }
-    
-    // ******************************* Approach 2 ******************************* 
     
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
+        List<List<Integer>> allSubsets = new ArrayList<>();
         
-        getSubSequences(0, nums, new ArrayList<>(), answer);
-        
-        return answer;
+        getAllSubset(0, nums, allSubsets, new ArrayList<>());
+        return allSubsets;
     }
     
-    public void getSubSequences(int i, int[] arr, ArrayList<Integer> list, List<List<Integer>> answer){
+    public void getAllSubset(int i, int[] arr, List<List<Integer>> answer, ArrayList<Integer> list){
         if (i == arr.length){
-            answer.add(new ArrayList<>(list));
+            answer.add(new ArrayList<Integer>(list));
             return;
         }
         
-        list.add(arr[i]);
-        getSubSequences(i+1, arr, list, answer);
-        list.remove(list.size() -1);
+        getAllSubset(i + 1, arr, answer, list);
         
-        getSubSequences(i+1, arr, list, answer);
+        list.add(arr[i]);
+        getAllSubset(i + 1, arr, answer, list);
+        list.remove(list.size() - 1);
     }
 }
