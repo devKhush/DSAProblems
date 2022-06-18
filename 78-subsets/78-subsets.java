2a7fbd1878d5cecb416d6158313e8ca744aa1ee2
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-18 23:58:12 +0530

    Time: 1 ms (86.38%), Space: 43.4 MB (49.11%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -3,6 +3,26 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> allSubsets = new ArrayList<>();
         
+        getAllSubsets(0, nums, allSubsets, new ArrayList<>());
+        return allSubsets;
+    }
+    
+    private void getAllSubsets(int index, int[] arr, List<List<Integer>> answer, ArrayList<Integer> list){
+        answer.add(new ArrayList<>(list));
+        
+        for (int i = index; i < arr.length; i++){
+            list.add(arr[i]);
+            getAllSubsets(i + 1, arr, answer, list);
+            list.remove(list.size() - 1);
+        }
+    }
+    
+    
+
+    // Approach 1 ******************************************************************************
+    public List<List<Integer>> subsets_V1(int[] nums) {
+        List<List<Integer>> allSubsets = new ArrayList<>();
+        
         getAllSubset(0, nums, allSubsets, new ArrayList<>());
         return allSubsets;
     }
