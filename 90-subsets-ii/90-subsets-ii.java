82478669e0a29edd15d9fb9422eded9a2332efd7
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-19 11:34:05 +0530

    Time: 2 ms (82.89%), Space: 43.4 MB (78.77%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,7 +1,30 @@
 class Solution {
     
+    // Approach 2: Pick & Non-pick by using HashSet to avoid Duplicate ******************************
+    public List<List<Integer>> subsetsWithDup(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> allUniqueSubsets = new ArrayList<>();
+
+        getAllSubsets(0, arr, allUniqueSubsets, new ArrayList<>());
+        return allUniqueSubsets;
+    }
+    
+    public void getAllSubsets(int index, int[] arr, List<List<Integer>> answer, ArrayList<Integer> list){
+        answer.add(new ArrayList<>(list));
+        
+        for (int i = index; i < arr.length; i++){
+            if (i > index  && arr[i] == arr[i-1])
+                continue;
+            list.add(arr[i]);
+            getAllSubsets(i + 1, arr, answer, list);
+            list.remove(list.size() - 1);
+        }
+    }
+    
+    
+    
     // Approach 1: Pick & Non-pick by using HashSet to avoid Duplicate ******************************
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    public List<List<Integer>> subsetsWithDup_V1(int[] nums) {
         Arrays.sort(nums);
         HashSet<ArrayList<Integer>> allSubsets = new HashSet<>();
         
