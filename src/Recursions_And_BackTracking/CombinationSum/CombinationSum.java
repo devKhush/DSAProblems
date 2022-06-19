1b0152e6244a8e1ab35e0767c39c3e782db71a17
devKhush999 <khushdev7838@gmail.com>
2022-06-19 17:26:55 +0530

    Combination Sum – 1

--- src/Recursions_And_BackTracking/CombinationSum/CombinationSum.java
+++ src/Recursions_And_BackTracking/CombinationSum/CombinationSum.java
@@ -15,6 +15,22 @@ keeping rest of the code same, we get the right answer but in the less recursive
 Because sometimes we get the target before reaching the last index, so once we get the target
 we need to stop at that moment and no need to check for further elements. As it's OR, once the
 base condition is satisfied, again inside that we need to check whether (target == 0) and confirm.
+
+NOTE: THIS IDEA WILL WORK ONLY WHEN THE ELEMENT OF THE ARRAY ARE POSITIVE, IN CASE OF NEGATIVE ELEMENTS OR ZEROS
+ELEMENTS, THIS IDEA WILL NOT WORK. (and hence "target == 0" must go inside the base case 'if' condition)
+ */
+
+/*
+Time Complexity: O(2^t * k)     where t is the target, k is the average length of each subset
+Reason: Assume if you were not allowed to pick a single element multiple times,
+every element will have a couple of options: 'pick' or 'not pick' which is 2^n different recursion calls,
+also assuming that the average length of every combination generated is k.
+(to put length k data structure into another data structure)
+Why not (2^n) but (2^t) (where n is the size of an array)?
+Assume that there is 1 and the target you want to reach is 10.
+So, 10 times you can “pick or not pick” an element.
+
+Space Complexity: O(k*x), k is the average length of each combination and x is the no. of combinations
  */
 
 public class CombinationSum {
