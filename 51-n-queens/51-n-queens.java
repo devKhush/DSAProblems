56642ce12a2b48ca42d1c38759604d98ce444add
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-20 23:15:47 +0530

    Time: 2 ms (97.67%), Space: 44.3 MB (81.43%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,4 +1,52 @@
 class Solution {
+    // Basic Approach Made Efiicient using Hashing
+    public List<List<String>> solveNQueens(int n) {
+        char[][] chessBoard = new char[n][n];
+        for (char[] row : chessBoard)
+            Arrays.fill(row, '.');
+
+        boolean[] hasQueenOnHorizontalLeft = new boolean[n];
+        boolean[] hasQueenOnUpperLeftDiagonal = new boolean[2*n - 1];
+        boolean[] hasQueenOnLowerLeftDiagonal = new boolean[2*n - 1];
+        List<List<String>> allValidQueenArrangements = new ArrayList<>();
+        
+        dfs(0, n, chessBoard, allValidQueenArrangements, hasQueenOnHorizontalLeft, hasQueenOnUpperLeftDiagonal, hasQueenOnLowerLeftDiagonal);
+        return allValidQueenArrangements;
+    }
+
+    private void dfs(int column, int n, char[][] board, List<List<String>> allQueenArrangements,
+                     boolean[] hasQueenOnHorizontalLeft, boolean[] hasQueenOnUpperLeftDiagonal,
+                     boolean[] hasQueenOnLowerLeftDiagonal){
+
+        if (column == n){
+            ArrayList<String> list = new ArrayList<>();
+            for (char[] row : board)
+                list.add(new String(row));
+            allQueenArrangements.add(list);
+            return;    
+        }
+        
+        for (int row = 0; row < n; row++){
+
+            if (!hasQueenOnHorizontalLeft[row]  && !hasQueenOnLowerLeftDiagonal[row + column] &&
+                    !hasQueenOnUpperLeftDiagonal[n - 1 + column - row]){
+                hasQueenOnHorizontalLeft[row] = true;
+                hasQueenOnLowerLeftDiagonal[column + row] = true;
+                hasQueenOnUpperLeftDiagonal[n - 1 + column - row] = true;
+                
+                board[row][column] = 'Q';
+                dfs(column +1 ,n, board, allQueenArrangements, hasQueenOnHorizontalLeft, hasQueenOnUpperLeftDiagonal, hasQueenOnLowerLeftDiagonal);
+                board[row][column] = '.';
+
+                hasQueenOnUpperLeftDiagonal[n - 1 + column - row] = false;
+                hasQueenOnLowerLeftDiagonal[column + row] = false;
+                hasQueenOnHorizontalLeft[row] = false;
+            }
+        }
+    }
+    
+    
+   /* // Basic Approach **********************************************************
    public List<List<String>> solveNQueens(int n) {
         char[][] chessBoard = new char[n][n];
         for (char[] row : chessBoard)
@@ -56,4 +104,5 @@ class Solution {
         }
         return true;
     }
+    */
 }
