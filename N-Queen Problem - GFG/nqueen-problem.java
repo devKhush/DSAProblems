3ba5b26b788907e75dcfd455b28479776027ad0a
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-20 23:58:20 +0530

    Added solution - LeetHub

--- N-Queen Problem - GFG/nqueen-problem.java
+++ N-Queen Problem - GFG/nqueen-problem.java
@@ -35,47 +35,58 @@ class GFG{
 
 class Solution{
     static ArrayList<ArrayList<Integer>> nQueen(int n) {
-        int[][] board = new int[n][n];
-            
+        char[][] board = new char[n][n];
+
         ArrayList<ArrayList<Integer>> allQueenPositions = new ArrayList<>();
-        
+
         dfs(0, n, board, allQueenPositions);
         return allQueenPositions;
     }
-    
-    static void dfs(int column, int n, int[][] board, ArrayList<ArrayList<Integer>> allQueenPositions){
+
+    static void dfs(int column, int n, char[][] board, ArrayList<ArrayList<Integer>> allQueenPositions){
         if (column == n){
             ArrayList<Integer> list = new ArrayList<>();
-            
+
             for (int j = 0; j < n; j++)
                 for (int i = 0; i < n; i++)
-                    if (board[i][j] == 1)
+                    if (board[i][j] == 'Q')
                         list.add(i + 1);
-                        
+
             allQueenPositions.add(list);
             return;
         }
-        
+
         for (int row = 0; row < n; row++){
             if (isQueenSafeAndCanPlace(row, column, board, n)){
-                board[row][column] = 1;
+                board[row][column] = 'Q';
                 dfs(column + 1, n, board, allQueenPositions);
-                board[row][column] = 0;
+                board[row][column] = '.';
             }
         }
     }
-    
-    static boolean isQueenSafeAndCanPlace(int rowNumber, int columnNumber, int[][] board, int n){
+
+    static boolean isQueenSafeAndCanPlace(int rowNumber, int columnNumber, char[][] board, int n){
+        // this function is doing three traversal, one on-left, on upper-left diagonal & on lower-left diagonal
+        // This will take O(3n) Time complexity. We can reduce this O(3n) time to O(1) using Hashing! (see other java file)
+        // We don't check vertically up, vertically down, horizontally right, diagonally up-right
+        // and diagonally down-right because we haven't put any 'Queen' at those locations.
+
+        // checking diagonally up-left whether the chess board contains any 'Queen', if 'Queen' is
+        // to be put at chessBoard[row][column]
         for (int row = rowNumber, column = columnNumber; row >= 0 && column >= 0; row--, column--)
-            if (board[row][column] == 1)
+            if (board[row][column] == 'Q')
                 return false;
-        
+
+        // checking horizontally left whether the chess board contains any 'Queen', if 'Queen' is
+        // to be put at chessBoard[row][column]
         for (int row = rowNumber, column = columnNumber; column >= 0; column--)
-            if (board[row][column] == 1)
+            if (board[row][column] == 'Q')
                 return false;
-        
+
+        // checking diagonally down-left whether the chess board contains any 'Queen', if 'Queen' is
+        // to be put at chessBoard[row][column]
         for (int row = rowNumber, column = columnNumber; row < n && column >= 0; row++, column--)
-            if (board[row][column] == 1)
+            if (board[row][column] == 'Q')
                 return false;
         return true;
     }
