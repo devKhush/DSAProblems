25497a4d1d61a5046d109ee4ca6e6a2c978a3da7
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-22 12:38:47 +0530

    Time: 2 ms (76.96%), Space: 55.7 MB (5.94%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,8 +1,10 @@
 class Solution {
     public void moveZeroes(int[] arr) {
         int n = arr.length;
-        
+
         int i = 0;
+        // If the current element is not 0, then we need to
+        // append it just in front of last non-zero element we found.
         for (int j = 0; j < n; j++){
             if (arr[j] != 0){
                 arr[i] = arr[j];
@@ -10,6 +12,9 @@ class Solution {
             }
         }
         
+        // After we have finished processing all elements of array.
+ 	    // All the non-zero elements are already at beginning of array.
+ 	    // We just need to fill remaining array with 0's as we have overwritten zeros in the beginning of array
         while (i < n)
             arr[i++] = 0;
     }
