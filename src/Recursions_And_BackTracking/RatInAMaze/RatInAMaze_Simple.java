47a8c2e6c8659a248afb8bc6875115611407e8a2
devKhush999 <khushdev7838@gmail.com>
2022-06-22 17:30:56 +0530

    Rat in a maze

--- src/Recursions_And_BackTracking/RatInAMaze/RatInAMaze_Simple.java
+++ src/Recursions_And_BackTracking/RatInAMaze/RatInAMaze_Simple.java
@@ -1,48 +1,26 @@
-// { Driver Code Starts
-// Initial Template for Java
+package Recursions_And_BackTracking.RatInAMaze;
+import java.util.ArrayList;
 
-import java.util.*;
-class Rat {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+// https://youtu.be/bLGZhJlt4y0
+// https://takeuforward.org/data-structure/rat-in-a-maze/
+// https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
 
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            int[][] a = new int[n][n];
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++) a[i][j] = sc.nextInt();
-
-            Solution obj = new Solution();
-            ArrayList<String> res = obj.findPath(a, n);
-            Collections.sort(res);
-            if (res.size() > 0) {
-                for (int i = 0; i < res.size(); i++)
-                    System.out.print(res.get(i) + " ");
-                System.out.println();
-            } else {
-                System.out.println(-1);
-            }
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
-// m is the given matrix and n is the order of matrix
-class Solution {
-     public ArrayList<String> findPath(int[][] maze, int n) {
-      // Below given are the directions & corresponding (di, dj) pairs, i.,e value by which by (i,j) changes
+public class RatInAMaze_Simple {
+    public ArrayList<String> findAllPathsToDestination(int[][] maze, int n) {
+        // Below given are the directions & corresponding (di, dj) pairs, i.,e value by which by (i,j) changes
         // Directions:     down, left, right, up
         String[] directions = {"D", "L", "R", "U"};
         int[] di =          {+1,   0,   0,   -1};
         int[] dj =          { 0,  -1,  +1,    0};
 
+        // Stores all the paths that Rat can take to reach destination
         ArrayList<String> allPaths = new ArrayList<>();
+        // visited boolean array
         boolean[][] visited = new boolean[n][n];
 
+        // Finding all paths starting from maze[0][0] to maze[n-1][n-1]
+        // we need to check first element in maze to be 1 because we are not checking inside recursion
+        // if maze[0][0] we can't even move a single step
         if (maze[0][0] == 1)
             findAllPathsToDestination(0, 0, maze, visited, n, "", allPaths, directions, di, dj);
         return allPaths;
@@ -51,11 +29,16 @@ class Solution {
     private void findAllPathsToDestination(int i, int j, int[][] maze, boolean[][] visited, int n,
                                            String path, ArrayList<String> allPaths, String[] directions,
                                            int[] di, int[] dj){
+        // If we reached destination, store the path taken into an arraylist
         if (i == n-1 && j == n-1){
             allPaths.add(path);
             return;
         }
 
+
+        // Moving Down, Left, Right & Up one by one in a for loop
+        // Marks current cell as visited, add the current direction into the 'path' String
+        // and move into that direction, after backtracking mark it as unvisited
         for (int index = 0; index < 4; index++){
             int nextI = i + di[index];
             int nextJ = j + dj[index];
