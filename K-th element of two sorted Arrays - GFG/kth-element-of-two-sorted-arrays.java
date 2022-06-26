e11a1b97de9f27795480cb04e8527777d88035c3
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-27 00:27:14 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
@@ -44,22 +44,28 @@ class GFG {
 
 
 class Solution {
-    public long kthElement( int a[], int b[], int n1, int n2, int k){
-     int m = a.length, n = b.length;
-        int i = 0,  j = 0;
-        int counter = 0;
-        int kthElement = -1;
+    public long kthElement( int A[], int B[], int n1, int n2, int k){
+        int m = A.length, n = B.length;
+        if (m > n) return kthElement(B, A, -1, -1,k);
+        
+        int low = Math.max(0, k-n), high = Math.min(k, m);
 
-        while (counter < k){
-            int valA = i < m ? a[i] : Integer.MAX_VALUE;
-            int valB = j < n ? b[j] : Integer.MAX_VALUE;
-
-            if (valA <= valB)
-                kthElement = a[i++];
-            else if (valA > valB)
-                kthElement = b[j++];
-            counter++;
-        }
-        return kthElement;
+        while (low <= high){
+            int cutA = (low + high)/2;
+            int cutB = k - cutA;
+            
+            int leftA = cutA == 0 ? Integer.MIN_VALUE : A[cutA - 1];
+            int leftB = cutB == 0 ? Integer.MIN_VALUE : B[cutB - 1];
+            int rightA = cutA == m ? Integer.MAX_VALUE : A[cutA];
+            int rightB = cutB == n ? Integer.MAX_VALUE : B[cutB];
+            
+            if (leftA <= rightB && leftB <= rightA)
+                return Math.max(leftA, leftB);
+            if (leftA > rightB)
+                high = cutA - 1;
+            else if (leftB > rightA)
+                low = cutA + 1;
+        }        
+        return 1l;
     }
 }
