c8f92a71be1c6e2abfa6d10c3007179394a98318
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-27 12:17:02 +0530

    Time: 45 ms (11.73%), Space: 54.1 MB (51.79%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -1,10 +1,32 @@
 class Solution {
-    public int minPartitions(String n) {
-        int maxNumber = 0;
+    public int minPartitions_Efficient(String n) {
+        int minNumberOfDeciBinaryNumber = 0;
         char[] number = n.toCharArray();
         
         for (char digit : number)
-            maxNumber = Math.max(maxNumber, digit - '0');
-        return maxNumber;
+            minNumberOfDeciBinaryNumber = Math.max(minNumberOfDeciBinaryNumber, digit - '0');
+        return minNumberOfDeciBinaryNumber;
+    }
+    
+     public int minPartitions(String n) {
+        int minNumberOfDeciBinaryNumber = 0;
+        char[] number = n.toCharArray();
+        
+        while (true){
+            boolean hasAtleastOneNonZeroDigit = false;
+            
+            for (int i = 0; i < number.length; i++){
+                if (number[i] != '0'){
+                    number[i]--;
+                    
+                    if (!hasAtleastOneNonZeroDigit)
+                        hasAtleastOneNonZeroDigit = number[i] != '0';
+                }
+            }
+            minNumberOfDeciBinaryNumber++;
+            if (!hasAtleastOneNonZeroDigit)
+                break;
+        }
+         return minNumberOfDeciBinaryNumber;
     }
 }
