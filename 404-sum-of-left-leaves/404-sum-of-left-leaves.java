05b75054d88bb42d4385a43975044b1ff2f2397e
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-29 13:03:57 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (48.16%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.java
@@ -17,8 +17,23 @@ class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         if (root == null)
             return 0;
-        return getLeftLeafNodeSum(root.left, root) + getLeftLeafNodeSum(root.right, root);
+        // return getLeftLeafNodeSum(root.left, root) + getLeftLeafNodeSum(root.right, root);
+        return getLeftLeafNodeSum_(root.left, true) + getLeftLeafNodeSum_(root.right, false);
     }
+    
+    private int getLeftLeafNodeSum_(TreeNode root, boolean isLeftTree){
+        if (root == null)
+            return 0;
+        
+        if (root.left == null  && root.right == null){
+            if (isLeftTree)
+                return root.val;
+            else
+                return 0;
+        }
+        return getLeftLeafNodeSum_(root.left, true) + getLeftLeafNodeSum_(root.right, false);
+    }
+    
     
     private int getLeftLeafNodeSum(TreeNode root, TreeNode parent){
         if (root == null)
@@ -32,4 +47,5 @@ class Solution {
         }
         return getLeftLeafNodeSum(root.left, root) + getLeftLeafNodeSum(root.right, root);
     }
+    
 }
