5b229ff8e6b68c55fe0a0f951b752efa2169f272
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-30 23:38:16 +0530

    Time: 3 ms (90.44%), Space: 47.8 MB (29.70%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,6 +10,45 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        return divideAndMerge(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode divideAndMerge(ListNode[] lists, int low, int high){
+        if (low > high)
+            return null;
+        if (low == high)
+            return lists[low];
+        
+        int mid = (low + high) /2;
+        
+        ListNode leftHalfMergedList = divideAndMerge(lists, low, mid);
+        ListNode rightHalfMergedList = divideAndMerge(lists, mid + 1, high);
+        return mergeTwoLists(leftHalfMergedList, rightHalfMergedList);
+    }
+    
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(0), ptr = dummy;
+        
+        while (l1 != null || l2 != null){
+            int l1_val = l1 != null ? l1.val : Integer.MAX_VALUE;
+            int l2_val = l2 != null ? l2.val : Integer.MAX_VALUE;
+            
+            ptr.next = l1_val <= l2_val ? l1 : l2;
+            ptr = ptr.next;
+            
+            if (l1_val <= l2_val)
+                l1 = l1 != null ? l1.next : null;
+            else
+                l2 = l2 != null ? l2.next : null;
+            
+            ptr.next = null;
+        }
+        return dummy.next;
+    }
+        
+    
+    // MIN HEAP *******************************************************************************
+    public ListNode mergeKLists_MinHeap(ListNode[] lists) {
         PriorityQueue<ListNode> minHeap = new PriorityQueue<>(new NodeComparator());
 
         for (ListNode head : lists)
