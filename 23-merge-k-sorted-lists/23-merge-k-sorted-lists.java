c2439294eb63562f5a2b65aaa44aaa2d0362e43d
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-06-30 20:19:21 +0530

    Time: 127 ms (18.37%), Space: 46.4 MB (80.57%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,35 +10,43 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] kLists) {
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(new NodeComparator());
+        if (kLists.length == 0)
+            return null;
+        ListNode head = kLists[0];
 
-        for (ListNode heads : kLists){
-            ListNode ptr = heads;
-            ListNode prev = null;
-            
-            while (ptr != null){                
-                prev = ptr;
-                minHeap.add(ptr);
-                ptr = ptr.next;
-                prev.next = null;
+        for (int i = 1; i < kLists.length; i++)
+            head = mergeTwoSortedList(head, kLists[i]);
 
-            }
+        return head;
+    }
+
+    private ListNode mergeTwoSortedList(ListNode head1, ListNode head2){
+        if (head1 == null)
+            return head2;
+        if (head2 == null)
+            return head1;
+        ListNode list1 = head1, list2 = head2;
+
+        if (list1.val > list2.val){
+            ListNode temp = list2;
+            list2 = list1;
+            list1 = temp;
         }
-        
-        ListNode head = minHeap.poll();
-        ListNode ptr = head;
-        
-        while (minHeap.size() != 0){
-            ptr.next = minHeap.remove();
-            ptr = ptr.next;
+        ListNode head = list1;
+
+        while (list1 != null  &&  list2 != null){
+            ListNode previous = null;
+
+            while (list1 != null && list1.val <= list2.val){
+                previous = list1;
+                list1 = list1.next;
+            }
+            previous.next = list2;
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
         }
         return head;
     }
-    
-    class NodeComparator implements Comparator<ListNode>{
-        @Override 
-        public int compare(ListNode a, ListNode b){
-            return a.val - b.val;
-        }
-    }
+
 }
