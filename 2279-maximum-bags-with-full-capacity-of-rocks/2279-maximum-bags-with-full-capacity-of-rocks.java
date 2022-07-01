a98892e2ca7fbced66f74e6a90a5070539d3f0b4
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-01 12:18:57 +0530

    Time: 18 ms (86.87%), Space: 90.5 MB (50.55%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,23 +1,41 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        Bag[] bags = new Bag[capacity.length];
+        int[] rocksNeededToFillBags = new int[capacity.length];
         
+        for (int i = 0; i < rocksNeededToFillBags.length; i++)
+            rocksNeededToFillBags[i] = capacity[i] - rocks[i];
+        
+        Arrays.sort(rocksNeededToFillBags);
+        
+        int bagsFilled = 0;
+        for (int i = 0; i < rocksNeededToFillBags.length; i++){
+            if (rocksNeededToFillBags[i] <= additionalRocks){
+                bagsFilled++;
+                additionalRocks -= rocksNeededToFillBags[i];
+            }
+            else
+                break;
+        }
+        return bagsFilled;
+    }
+    
+    
+    
+    public int maximumBags_Greedy(int[] capacity, int[] rocks, int additionalRocks) {
+        Bag[] bags = new Bag[capacity.length];
         for (int i = 0; i < bags.length; i++)
             bags[i] = new Bag(capacity[i], rocks[i]);
         
         Arrays.sort(bags, new BagComparator());
-        
         int filledBags = 0;
         
         for (int i = 0; i < bags.length; i++){
             Bag bag = bags[i];
-            
             if (bag.capacity - bag.rock <= additionalRocks){
                 filledBags++;
                 additionalRocks -= bag.capacity - bag.rock;
             }
-            else
-                break;
+            else break;
         }
         return filledBags;
     }
