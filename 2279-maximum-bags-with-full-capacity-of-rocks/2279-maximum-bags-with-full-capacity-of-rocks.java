323b0c5c389d25e126e9d3c9496270ff1afbe297
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-01 11:34:11 +0530

    Time: 105 ms (20.08%), Space: 97.4 MB (23.65%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        Bag[] bags = new Bag[capacity.length];
+        
+        for (int i = 0; i < bags.length; i++)
+            bags[i] = new Bag(capacity[i], rocks[i]);
+        
+        Arrays.sort(bags, new BagComparator());
+        
+        int filledBags = 0;
+        
+        for (int i = 0; i < bags.length; i++){
+            Bag bag = bags[i];
+            
+            if (bag.capacity - bag.rock <= additionalRocks){
+                filledBags++;
+                additionalRocks -= bag.capacity - bag.rock;
+            }
+            else
+                break;
+        }
+        return filledBags;
+    }
+    
+    class Bag{
+        int capacity, rock;
+        public Bag(int capacity, int rock){
+            this.capacity = capacity;
+            this.rock = rock;
+        }
+    }
+    
+    class BagComparator implements Comparator<Bag>{
+        @Override
+        public int compare(Bag a, Bag b){
+            return (a.capacity - a.rock) - (b.capacity - b.rock);
+        }
+    }
+}
