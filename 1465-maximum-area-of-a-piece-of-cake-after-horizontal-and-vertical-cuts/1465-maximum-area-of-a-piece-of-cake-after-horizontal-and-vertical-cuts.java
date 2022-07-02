554766dec9a495fdeb3b0e1fe018dd04b9ed97b4
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-02 12:22:34 +0530

    Time: 16 ms (98.49%), Space: 61 MB (39.47%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,7 +1,8 @@
 class Solution {
     private int mod = 1000000007;
-
-    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+    
+    // Main Solution ***********************************************************
+    public int maxArea_V1(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
         Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
         
@@ -20,4 +21,25 @@ class Solution {
         // return (int) (((maxPieceWidth % mod) * (maxPieceHeight % mod)) % mod);
         return (int) ((maxPieceWidth * maxPieceHeight) % mod);
     }
+    
+    
+    // Compact Code
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        long maxCakePieceHeight = getMaxLengthOfInterval(h, horizontalCuts);
+        long maxCakePieceWidth = getMaxLengthOfInterval(w, verticalCuts);
+        
+        return (int) ((maxCakePieceHeight * maxCakePieceWidth) % mod);
+    }
+    
+    private int getMaxLengthOfInterval(int totalLength, int[] intervals){
+        Arrays.sort(intervals);
+        
+        int maxIntervalLength = Math.max(intervals[0], totalLength - intervals[intervals.length - 1]);
+        
+        for (int i = 0; i < intervals.length - 1; i++)
+            maxIntervalLength = Math.max(intervals[i + 1] - intervals[i], maxIntervalLength);
+        
+        return maxIntervalLength;
+    }
+    
 }
