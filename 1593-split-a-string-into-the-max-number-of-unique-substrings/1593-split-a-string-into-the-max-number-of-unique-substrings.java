a1e2a2206dc34b4447c96ba531bf834ef92b77b8
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-07 22:51:19 +0530

    Time: 110 ms (22.03%), Space: 70.7 MB (14.98%) - LeetHub

---
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+    private int maximumPartitions;
+    
+    public int maxUniqueSplit(String s) {
+        maximumPartitions = Integer.MIN_VALUE;
+        
+        partitionStringIntoSubString(0, s, s.length(), new HashSet<>(), 0);
+        return maximumPartitions;
+    }
+    
+    
+    public void partitionStringIntoSubString(int index, String s, int n, HashSet<String> subStringPartitions, int partitionsDone){
+        if (index == n){
+            maximumPartitions = Math.max(partitionsDone, maximumPartitions);
+            return;
+        }
+        
+        for (int i = index; i < n; i++){
+            String subString = s.substring(index, i + 1);
+            
+            if (!subStringPartitions.contains(subString)){
+                subStringPartitions.add(subString);
+                partitionStringIntoSubString(i + 1, s, n, subStringPartitions, partitionsDone + 1);
+                subStringPartitions.remove(subString);
+            }
+        }
+    }
+}
