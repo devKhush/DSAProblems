b64f29a6ddf807f946a19645c240ab6936e4f770
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-10 23:14:10 +0530

    Time: 1 ms (89.45%), Space: 45 MB (7.58%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        
+        return climbStairs(n, cost, dp);
+    }
+    
+    // MEMOIZATION ***************************************************************************
+    public int climbStairs(int index, int[] cost, int[] dp){
+        if (index == 0 || index == 1)
+            return dp[index] = 0;
+        
+        if (dp[index] != -1)
+            return dp[index];
+        
+        int costTwoStepBehind = Integer.MAX_VALUE;
+        if (index >= 2)
+            costTwoStepBehind = climbStairs(index - 2, cost, dp) + cost[index - 2];
+        
+        int costOneStepBehind = Integer.MAX_VALUE;
+        if (index >= 1)
+            costOneStepBehind = climbStairs(index - 1, cost, dp) + cost[index - 1];
+        
+        return dp[index] = Math.min(costTwoStepBehind, costOneStepBehind);
+    }
+    
+    // RECURSION ***************************************************************************
+    public int climbStairs_Recursion(int index, int[] cost){
+        if (index == 0 || index == 1)
+            return 0;
+        
+        int costTwoStepBehind = Integer.MAX_VALUE;
+        if (index >= 2)
+            costTwoStepBehind = climbStairs_Recursion(index - 2, cost) + cost[index - 2];
+        
+        int costOneStepBehind = Integer.MAX_VALUE;
+        if (index >= 1)
+            costOneStepBehind = climbStairs_Recursion(index - 1, cost) + cost[index - 1];
+        
+        return Math.min(costTwoStepBehind, costOneStepBehind);
+    }
+}
