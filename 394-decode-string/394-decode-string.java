7cefcdf2a3e329ae891b378d0e9b805b8df01d50
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-11 20:17:57 +0530

    Time: 1 ms (87.79%), Space: 41.9 MB (69.62%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,43 +1,37 @@
 class Solution {
     public String decodeString(String s) {
+        int n = s.length();
+        
         Stack<String> results = new Stack<>();
         Stack<Integer> counts = new Stack<>();
+        StringBuilder decodedString = new StringBuilder("");
+        int number = 0;
         
-        String decodedString = "";
-        int n = s.length();
-        int i = 0;
-        
-        while (i < n){
-            if (Character.isDigit(s.charAt(i))){
-                int number = 0;
-                
-                while (Character.isDigit(s.charAt(i))){
-                    number = number * 10 + (s.charAt(i) - '0');
-                    i++;
-                }
-                i--;
+        for (int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            
+            if (ch >= '0' && ch <= '9')
+                number = number * 10  + (ch - '0');
+            
+            else if (ch == '['){
+                results.push(decodedString.toString());
                 counts.push(number);
+                number = 0;
+                decodedString = new StringBuilder();
             }
-            
-            else if (s.charAt(i) == '['){
-                results.push(decodedString);
-                decodedString = "";
-            }
-            
-            else if (s.charAt(i) == ']'){
-                StringBuilder sb = new StringBuilder(results.pop());
-                int repeatCount = counts.pop();
+            else if (ch == ']'){
+                int repeat = counts.pop();
+                String previousResult = results.pop();
                 
-                while (repeatCount-- > 0)
-                    sb.append(decodedString);
-                
-                decodedString = sb.toString();
+                String currentResult = decodedString.toString();
+                decodedString = new StringBuilder(previousResult);
+                while (repeat-- > 0)
+                    decodedString.append(currentResult);
             }
-            
             else
-                decodedString += s.charAt(i);
-            i++;
+                decodedString.append(ch);
         }
-        return decodedString;
+        
+        return decodedString.toString();
     }
 }
