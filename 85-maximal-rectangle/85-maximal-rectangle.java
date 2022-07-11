20b4ec4bddb45dc1e1efd3b3bbf17b3c6331c805
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-11 23:07:31 +0530

    Time: 6 ms (93.47%), Space: 54.9 MB (41.55%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int[] histogram = new int[n];
+        int maxRectangleArea = 0;
+        
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (matrix[i][j] == '1')
+                    histogram[j]++;
+                else
+                    histogram[j] = 0;
+            }
+            maxRectangleArea = Math.max(maximumAreaInHistogram(histogram, n), maxRectangleArea);
+        }
+        return maxRectangleArea;
+    }
+    
+    public int maximumAreaInHistogram(int[] arr, int n){
+        int[] PSE = new int[n];
+        int[] NSE = new int[n];
+        
+        PSE[0] = -1;
+        NSE[n - 1] = n;
+        
+        for (int i = 1; i < n; i++){
+            int low = i - 1;
+            
+            while (low >= 0  &&  arr[low] >= arr[i])
+                low = PSE[low];
+            
+            PSE[i] = low;
+        }
+        
+        for (int i = n - 1; i >= 0; i--){
+            int high = i + 1;
+            
+            while (high < n  &&  arr[i] <= arr[high])
+                high = NSE[high];
+            
+            NSE[i] = high;
+        }
+        
+        int maxAreaInHistogram = 0;
+        
+        for (int i = 0; i < n; i++){
+            int width = NSE[i] - PSE[i] - 1;
+            int height = arr[i];
+            maxAreaInHistogram = Math.max(height * width, maxAreaInHistogram);
+        }
+        return maxAreaInHistogram;
+    }
+}
