30f499fe0887d18f1a64c15c6eb7c0c97051e7b1
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-12 15:53:28 +0530

    Time: 3 ms (42.24%), Space: 42.1 MB (43.81%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,5 +1,31 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int student : students)
+            queue.add(student);
+
+        int i = 0;
+        int sentToBackOfQueueCount = 0;
+
+        while (!queue.isEmpty()  &&  sentToBackOfQueueCount != queue.size()){
+            int currentStudentPreference = queue.remove();
+
+            if (currentStudentPreference == sandwiches[i]){
+                i++;
+                sentToBackOfQueueCount = 0;
+            }
+            else{
+                sentToBackOfQueueCount++;
+                queue.add(currentStudentPreference);
+            }
+        }
+        return queue.size();
+    }
+    
+    
+    
+    public int countStudents_Solution1(int[] students, int[] sandwiches) {
         Queue<Integer> queue = new ArrayDeque<>();
         
         for (int student : students)
