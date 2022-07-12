355a3ee1764d30f0bea035dd4cbac0bef06511b4
devKhush999 <khushdev7838@gmail.com>
2022-07-12 11:05:02 +0530

    Moving average

--- src/Queues/MovingAverageFromDataStream/MovingAverageFromDataStream.java
+++ src/Queues/MovingAverageFromDataStream/MovingAverageFromDataStream.java
@@ -3,26 +3,26 @@ package Queues.MovingAverageFromDataStream;
 // https://www.lintcode.com/problem/642/
 // https://www.youtube.com/watch?v=E-kjYOZEBxY
 
-import java.util.LinkedList;
+
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class MovingAverageFromDataStream {
     private Queue<Integer> queue;
-    private int size;
-    private double sum;
+    private int size, sum;
 
     public MovingAverageFromDataStream(int size) {
-        queue = new LinkedList<>();
+        this.queue = new ArrayDeque<>();
         this.sum = 0;
         this.size = size;
     }
 
     public double next(int val) {
-        if (queue.size() == this.size){
-            this.sum -= queue.remove();
-        }
-        this.sum += val;
-        this.queue.add(val);
-        return (this.sum + 0.0)/this.queue.size();
+        if (queue.size() == size)
+            sum -= queue.remove();
+
+        sum += val;
+        queue.add(val);
+        return sum / (double) queue.size();
     }
 }
