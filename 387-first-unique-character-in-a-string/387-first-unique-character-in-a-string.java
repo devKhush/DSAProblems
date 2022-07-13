83c2046165ba8e7b49aa9a049e2dc674acbca6fc
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-13 14:25:56 +0530

    Time: 12 ms (76.44%), Space: 48.7 MB (60.99%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,5 +1,23 @@
 class Solution {
     public int firstUniqChar(String s) {
+        int[] count = new int[26];
+
+        Queue<Integer> nonRepeatingCharIndices = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            count[ch - 'a']++;
+
+            if (count[ch - 'a'] == 1)
+                nonRepeatingCharIndices.add(i);
+
+            while (!nonRepeatingCharIndices.isEmpty()  &&  count[s.charAt(nonRepeatingCharIndices.peek()) - 'a'] != 1)
+                nonRepeatingCharIndices.remove();
+        }
+        return !nonRepeatingCharIndices.isEmpty()? nonRepeatingCharIndices.peek() : -1;
+    }
+    
+    public int firstUniqChar_Hashing(String s) {
         int n = s.length();
         int[] count = new int[26];
         
