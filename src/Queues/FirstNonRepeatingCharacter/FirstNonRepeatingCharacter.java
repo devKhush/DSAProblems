9d74a81818c56457f18a90ec436959a23ec21062
devKhush999 <khushdev7838@gmail.com>
2022-07-13 15:40:59 +0530

    First non-repeating char

--- src/Queues/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.java
+++ src/Queues/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.java
@@ -4,53 +4,64 @@ import java.util.HashMap;
 // https://www.youtube.com/watch?v=5co5Gvp_-S0
 
 class FirstNonRepeatingCharacter {
+    /* ******************************* Brute Force ********************************************
+     * Intuition: For each character in the string check whether it is the first one or not.
+                  If it is the first non-repeating character, return the index
+     * Time Complexity: O(n^2)
+     * Space Complexity: O(1)
+     */
+    public int firstUniqChar_BruteForce(String s) {
+        int n = s.length();
 
-    // O(n^2) Brute force
-    public int firstUniqChar_O_NSquare(String s) {
-        
-        for (int i=0; i<s.length(); i++){
-            boolean seenDuplicate = false;
-            
-            for (int j = 0; j<s.length(); j++){
-                if (i!=j && s.charAt(i)==s.charAt(j)){
-                    seenDuplicate = true;
-                    break;
-                }
-            }
-            if (!seenDuplicate)             // if (j==s.length())    condition also works
-                return i;
-        }
-        
-        return -1;
-    }
-
-    // O(n)
-    public int firstUniqChar_2(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (int i=0; i<s.length(); i++){
+        // Checking for each character whether it is duplicate or not
+        for (int i = 0; i < n; i++){
             char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0)+1);
-        }
-        for (int i=0; i<s.length(); i++){
-            if (map.get(s.charAt(i)) == 1)
+
+            // checking in the whole String whether "Current character" is the first non-repeating char or not
+            int j;
+            for (j = 0; j < n; j++)
+                if (i != j  &&  ch == s.charAt(j))      // if it is repeating, it is definitely not the first non-repeating char
+                    break;
+
+            if (j == n)
                 return i;
         }
         return -1;
     }
 
-    // DP Approach (much faster)
-    public int firstUniqChar_3(String s) {
-        char[] charCount = new char[26];
-        for (int i=0; i<s.length(); i++)
+
+    // ************************************ Hashing Solution ******************************************
+    // Time Complexity :  O(n) + O(n)  =  O(n)
+    // Space Complexity:  O(26) = O(1)
+    public int firstUniqChar_HashMap(String s) {
+        int n = s.length();
+        HashMap<Character, Integer> characterCountMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            characterCountMap.put(ch, characterCountMap.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < n; i++)
+            if (characterCountMap.get(s.charAt(i)) == 1)
+                return i;
+        return -1;
+    }
+
+
+    // ******************************** Hashing using Array Solution **********************************
+    // Time Complexity :  O(n) + O(n)  =  O(n)
+    // Space Complexity:  O(26) = O(1)
+    public int firstUniqChar_ArrayHashing(String s) {
+        int n = s.length();
+        int[] charCount = new int[26];
+
+        for (int i = 0; i < n; i++)
             charCount[s.charAt(i) - 'a']++;
 
-        for (int i=0; i<s.length(); i++){
+        for (int i = 0; i < n; i++)
             if (charCount[s.charAt(i) - 'a'] == 1)
                 return i;
-        }
 
         return -1;
     }
-
 }
--- src/Queues/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter_UsingQueue.java
+++ src/Queues/FirstNonRepeatingCharacter/FirstNonRepeatingCharacter_UsingQueue.java
@@ -1,4 +1,5 @@
 package Queues.FirstNonRepeatingCharacter;
+import java.util.ArrayDeque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -6,38 +7,74 @@ import java.util.Queue;
 // https://www.youtube.com/watch?v=2Z--YYbzigU&t=195s
 
 public class FirstNonRepeatingCharacter_UsingQueue {
-    public int firstUniqChar(String s) {
-        // stores the count of the characters
-        HashMap<Character, Integer> count = new HashMap<>();
+    /*
+    * Intuition: We will maintain the first non-repeating character on the front of the queue.
+                  And will keep on removing the character from the queue if their count becomes more than 1.
+    * Time Complexity: O(2 * n)  =  O(n)
+    * Space Complexity: O(n) + O(n) = O(n)
+     */
+    public int firstUniqChar_ExtendedQuestion(String s) {
+        // HashMap/ArrayMap to stores the count of the characters
+        int[] count = new int[26];
 
-        // queue stores the first non-repeating characters
-        Queue<Character> queue = new LinkedList<>();
+        // The Queue will store the first non-repeating characters
+        // Since we need first non-repeating character, we use Queue as it follows FIFO principle
+        Queue<Character> nonRepeatingCharQueue = new LinkedList<>();
 
-        for (int i=0; i<s.length(); i++){
+        for (int i = 0; i < s.length(); i++){
+            // Maintains the count of all characters
             char ch = s.charAt(i);
-            count.put(ch, count.getOrDefault(ch, 0)+1);
+            count[ch - 'a']++;
 
-            if (count.get(ch) == 1)
-                queue.add(ch);
+            // If frequency of current char is 1 (character is newly found), add it into the queue
+            // as it can be first non-repeating character for later indices.
+            if (count[ch - 'a'] == 1)
+                nonRepeatingCharQueue.add(ch);
 
-            while (!queue.isEmpty()  &&  count.get(queue.peek()) != 1)
-                queue.remove();
+            // Removing all repeating characters
+            while (!nonRepeatingCharQueue.isEmpty()  &&  count[nonRepeatingCharQueue.peek() -'a'] != 1)
+                nonRepeatingCharQueue.remove();
 
-
-            // To print the first non-repeating characters, some extra part of
+            // To print the first non-repeating characters at each index, some extra part of
             // this question, can be counted as next level problem
-             if (!queue.isEmpty())
-                 System.out.print(queue.peek() + " ");
+             if (!nonRepeatingCharQueue.isEmpty())
+                 System.out.print(nonRepeatingCharQueue.peek() + " ");
              else
                  System.out.print(-1 + " ");
         }
-
-        return !queue.isEmpty() ? s.indexOf(queue.peek()) : -1;
+        return !nonRepeatingCharQueue.isEmpty() ? s.indexOf(nonRepeatingCharQueue.peek()) : -1;
     }
+
+
+    /*
+    * Intuition: We will maintain the first non-repeating character on the front of the queue.
+                  And will keep on removing the character from the queue if their count becomes more than 1.
+    * This is same as leetcode version of above Question
+    * Time Complexity: O(2 * n)  =  O(n)
+    * Space Complexity: O(n) + O(n) = O(n)
+    */
+    public int firstUniqChar_LeetCode(String s) {
+        int[] count = new int[26];
+
+        Queue<Integer> nonRepeatingCharIndices = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            count[ch - 'a']++;
+
+            if (count[ch - 'a'] == 1)
+                nonRepeatingCharIndices.add(i);
+
+            while (!nonRepeatingCharIndices.isEmpty()  &&  count[s.charAt(nonRepeatingCharIndices.peek()) - 'a'] != 1)
+                nonRepeatingCharIndices.remove();
+        }
+        return !nonRepeatingCharIndices.isEmpty()? nonRepeatingCharIndices.peek() : -1;
+    }
+
 
     public static void main(String[] args) {
         String s = "aabbcdddakgsac";
-        int answer = new FirstNonRepeatingCharacter_UsingQueue().firstUniqChar(s);
+        int answer = new FirstNonRepeatingCharacter_UsingQueue().firstUniqChar_ExtendedQuestion(s);
         System.out.println();
 
         System.out.println(answer);
