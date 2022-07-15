288dd54e5ef5842d4b4f4a898db3e1025efc4584
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-15 23:49:18 +0530

    Time: 0 ms (100.00%), Space: 54.6 MB (7.31%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,5 +1,37 @@
 class Solution {
+    // DFS SOlution ********************************************************************************
     public boolean isBipartite(int[][] adjList) {
+        int V = adjList.length;
+        int[] color = new int[V];
+
+        for (int vertex = 0; vertex < V; vertex++)
+            if (color[vertex] == 0) {
+                color[vertex] = 1;
+
+                if (!canDoTwoColoring_DFS(vertex, adjList, color, V))
+                    return false;
+            }        
+        return true;
+    }
+
+    public boolean canDoTwoColoring_DFS(int vertex, int[][] adjList, int[] color, int V){
+
+        for (int adjacentVertex : adjList[vertex]){
+            if (color[adjacentVertex] == 0){
+                color[adjacentVertex] = color[vertex] == 1 ? 2 : 1;
+
+                if (!canDoTwoColoring_DFS(adjacentVertex, adjList, color, V))
+                    return false;
+            }
+            else if (color[vertex] == color[adjacentVertex])
+                return false;
+        }
+        return true;
+    }
+    
+    
+    // BFS Solution ********************************************************************************
+    public boolean isBipartite_BFS(int[][] adjList) {
         int V = adjList.length;
         int[] color = new int[V];
         
