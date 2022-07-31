9467f2fb43b170f97f83fbba3d094e6ca343084a
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-31 16:52:50 +0530

    Time: 56 ms (10.92%), Space: 43.4 MB (53.80%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -1,4 +1,4 @@
-/*
+    /*
 // Definition for a Node.
 class Node {
     public int val;
@@ -19,32 +19,62 @@ class Node {
 */
 
 class Solution {
-    public Node cloneGraph(Node node) {
-       if (node == null)
-            return null;
+    public void dfs(Node vertex, HashMap<Integer, Node> visited){
+        Node vertexInNewGraph = visited.get(vertex.val);
         
-        Node[] nodeMap = new Node[101];
-        Queue<Node> bfsQueue = new ArrayDeque<>();
-        
-        int startVertex = node.val;
-        
-        nodeMap[startVertex] = new Node(startVertex);
-        bfsQueue.add(node);
-        
-        while (!bfsQueue.isEmpty()){
-            Node vertex = bfsQueue.remove();
-            Node vertexInNewGraph = nodeMap[vertex.val];
-
-            for (Node neighbour : vertex.neighbors){
-                if (nodeMap[neighbour.val] == null){
-                    nodeMap[neighbour.val] = new Node(neighbour.val);
-                    bfsQueue.add(neighbour);
-                }
+        for (Node neighbour : vertex.neighbors){
+            if (!visited.containsKey(neighbour.val)){
+                visited.put(neighbour.val, new Node(neighbour.val));
+                Node adjacentInNewGraph = visited.get(neighbour.val);
+                adjacentInNewGraph.neighbors.add(vertexInNewGraph);
                 
-                Node adjacentInNewGraph = nodeMap[neighbour.val];
+                dfs(neighbour, visited);
+            }
+            else{
+                Node adjacentInNewGraph = visited.get(neighbour.val);
                 adjacentInNewGraph.neighbors.add(vertexInNewGraph);
             }
         }
-        return nodeMap[startVertex]; 
+    }
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        HashMap<Integer, Node> visited = new HashMap<>();
+        
+        int startVertex = node.val;
+        visited.put(startVertex, new Node(startVertex));
+        dfs(node, visited);
+        return visited.get(startVertex);
+    }
+    
+    
+    public Node cloneGraph_BFS(Node node) {
+       if (node == null)
+            return null;
+        
+        HashMap<Integer, Node> visited = new HashMap<>();
+        
+        int startVertex = node.val;
+        visited.put(startVertex, new Node(startVertex));
+
+        Queue<Node> bfsQueue = new ArrayDeque<>();
+        bfsQueue.add(node);
+
+        while (!bfsQueue.isEmpty()){
+            Node vertex = bfsQueue.remove();
+            Node vertexInNewGraph = visited.get(vertex.val);
+
+            for (Node neighbour : vertex.neighbors){
+                if (!visited.containsKey(neighbour.val)){
+                    visited.put(neighbour.val, new Node(neighbour.val));
+                    bfsQueue.add(neighbour);
+                }
+
+                Node adjacentInNewGraph = visited.get(neighbour.val);
+                adjacentInNewGraph.neighbors.add(vertexInNewGraph);
+            }
+        }
+        return visited.get(startVertex);
     }
 }
