3ed0070eea141ffacb7962aa9a417fa6b055faa4
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-31 20:23:31 +0530

    Time: 5 ms (56.10%), Space: 49.9 MB (98.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,24 +1,39 @@
 class Solution {
-    public void dfs(int i, int j, int m, int n, char[][] grid){
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0')
-            return;
+   public void bfs(int i, int j, int m, int n, char[][] grid){
+        int[] dx = {0,  0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        Queue<int[]> bfsQueue = new ArrayDeque<>();
+        bfsQueue.add(new int[]{i, j});
         
         grid[i][j] = '0';
-        
-        dfs(i - 1, j, m, n, grid);
-        dfs(i, j + 1, m, n, grid);
-        dfs(i + 1, j, m, n, grid);
-        dfs(i, j - 1, m, n, grid);
+
+        while (!bfsQueue.isEmpty()){
+            i  = bfsQueue.peek()[0];
+            j  = bfsQueue.peek()[1];
+            bfsQueue.remove();
+
+            for (int a = 0; a < 4; a++){
+                int nextI = i + dx[a];
+                int nextJ = j + dy[a];
+
+                if (nextI >= 0  &&  nextJ >= 0  && nextI < m  &&  nextJ < n  && grid[nextI][nextJ] == '1') {
+                    bfsQueue.add(new int[]{nextI, nextJ});
+                    grid[nextI][nextJ] = '0';
+                }
+            }
+        }
     }
+    
     public int numIslands(char[][] grid) {
-         int m = grid.length, n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
         int numberOfIslands = 0;
 
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
                 if (grid[i][j] == '1'){
                     numberOfIslands++;
-                    dfs(i, j, m, n, grid);
+                    bfs(i, j, m, n, grid);
                 }
             }
         }
