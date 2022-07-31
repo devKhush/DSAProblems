70d6086be2bed9d888dba1fe167ffa9671abd69f
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-07-31 15:35:13 +0530

    Time: 39 ms (60.20%), Space: 43 MB (77.76%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -20,39 +20,31 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        if (node == null)
+       if (node == null)
             return null;
         
-        HashMap<Integer, Node> nodeMap = new HashMap<>();
-        HashSet<Integer> visited = new HashSet<>();
+        Node[] nodeMap = new Node[101];
         Queue<Node> bfsQueue = new ArrayDeque<>();
         
         int startVertex = node.val;
-        Node copyNode = new Node(startVertex);
         
-        visited.add(startVertex);
-        nodeMap.put(startVertex, copyNode);
+        nodeMap[startVertex] = new Node(startVertex);
         bfsQueue.add(node);
         
         while (!bfsQueue.isEmpty()){
             Node vertex = bfsQueue.remove();
-            Node vertexInNewGraph = nodeMap.get(vertex.val);
+            Node vertexInNewGraph = nodeMap[vertex.val];
 
-            
             for (Node neighbour : vertex.neighbors){
-                if (!nodeMap.containsKey(neighbour.val))
-                    nodeMap.put(neighbour.val, new Node(neighbour.val));
-                
-                Node adjacentInNewGraph = nodeMap.get(neighbour.val);
-                adjacentInNewGraph.neighbors.add(vertexInNewGraph);
-                
-                if (!visited.contains(neighbour.val)){
+                if (nodeMap[neighbour.val] == null){
+                    nodeMap[neighbour.val] = new Node(neighbour.val);
                     bfsQueue.add(neighbour);
-                    visited.add(neighbour.val);
                 }
+                
+                Node adjacentInNewGraph = nodeMap[neighbour.val];
+                adjacentInNewGraph.neighbors.add(vertexInNewGraph);
             }
         }
-        return nodeMap.get(startVertex);
-        
+        return nodeMap[startVertex]; 
     }
 }
