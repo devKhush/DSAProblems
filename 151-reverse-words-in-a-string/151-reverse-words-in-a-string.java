d97984008895c2068b8cbb4ef5aab10440114a0c
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-03 23:16:38 +0530

    Time: 12 ms (53.04%), Space: 44.6 MB (36.14%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,22 +1,47 @@
 class Solution {
     public String reverseWords(String s) {
+        s = s.trim() + " ";
+        int n = s.length();
+
+        StringBuilder answer = null;
+        StringBuilder currWord = new StringBuilder();
+
+        for (int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+
+            if (ch != ' ')
+                currWord.append(ch);
+
+            else{
+                if (currWord.toString().equals(""))
+                    continue;
+                if (answer != null)
+                    answer.insert(0, currWord + " ");
+                else
+                    answer = new StringBuilder(currWord.toString());
+
+                currWord = new StringBuilder();
+            }
+        }
+        return answer.toString();
+    }
+    
+    
+    public String reverseWords_Stack(String s) {
         StringBuilder sb = new StringBuilder();
         s = " " + s;
         int n = s.length();
-        
         Stack<Character> stack = new Stack<>();
         
         for (int i = n - 1; i >= 0; i--){
             char ch = s.charAt(i);
-            
+
             if (ch != ' ')
                 stack.push(ch);
             else{
                 boolean stackWasNotEmpty = !stack.isEmpty();
-                
                 while (!stack.isEmpty())
-                    sb.append(stack.pop());
-                
+                    sb.append(stack.pop());                
                 if (stackWasNotEmpty)
                     sb.append(" ");
             }
