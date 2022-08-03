fdad90bf5492a865c01eb14fb60df53dd9a7c41e
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-03 16:45:14 +0530

    Time: 118 ms (21.34%), Space: 54.2 MB (71.10%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -0,0 +1,93 @@
+class MyCalendar {
+    private ArrayList<int[]> bookings;
+    
+    public MyCalendar() {
+        this.bookings = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        for (int[] booking : bookings){
+            if (Math.max(start, booking[0]) < Math.min(end, booking[1]))
+                return false;
+        }
+        
+        bookings.add(new int[]{start, end});
+        return true;
+    }
+}
+
+
+// Binary Search ******************************************************************************
+class MyCalendar_BinarySearch {
+    private ArrayList<int[]> bookings;
+
+    public MyCalendar_BinarySearch() {
+        this.bookings = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        if (bookings.size() == 0){
+            bookings.add(new int[]{start, end});
+            return true;
+        }
+        
+        int low = 0, high = bookings.size() - 1;
+        
+        while (low <= high){
+            int mid = (low + high) >> 1;
+            
+            if (bookings.get(mid)[0] <= start)
+                low = mid + 1;
+            else if (bookings.get(mid)[0] > start)
+                high = mid - 1;
+        }
+        int insertPosition = low;
+        
+        if (insertPosition == 0)
+            return insertAtFront(start, end);
+        
+        if (insertPosition == bookings.size())
+            return insertAtEnd(start, end);
+        
+        return insert(start, end, insertPosition);
+    }
+    
+    private boolean insert(int start, int end, int insertPosition){
+        int[] previousBooking = bookings.get(insertPosition - 1);
+        int[] nextBooking = bookings.get(insertPosition);
+        
+        if (start < previousBooking[1])
+            return false;
+        if (end > nextBooking[0])
+            return false;
+        
+        bookings.add(insertPosition, new int[]{start, end});
+        return true;
+    }
+    
+    private boolean insertAtFront(int start, int end){
+        int[] nextBooking = bookings.get(0);    
+        
+        if (end > nextBooking[0])
+            return false;
+        
+        bookings.add(0, new int[]{start, end});
+        return true;
+    }
+    
+    private boolean insertAtEnd(int start, int end){
+        int[] previousBooking = bookings.get(bookings.size() - 1);
+        
+        if (start < previousBooking[1])
+            return false;
+        
+        bookings.add(new int[]{start, end});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
