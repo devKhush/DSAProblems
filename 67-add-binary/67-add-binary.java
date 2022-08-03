ba9e2c8dee1235e3b5a222df07230d154ae7b69d
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-03 20:20:38 +0530

    Time: 3 ms (68.65%), Space: 42.3 MB (82.32%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,5 +1,28 @@
 class Solution {
     public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        
+        int i = a.length() - 1, j = b.length() - 1;        
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1){
+            int sum = 0;
+            sum += carry;
+            
+            if (i >= 0) 
+                sum += a.charAt(i--) - '0';
+            if (j >= 0)
+                sum += b.charAt(j--) - '0';
+
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        return sb.reverse().toString();
+    }
+    
+    
+    // String Concatenation is usually Too Slow
+    public String addBinary_Brute(String a, String b) {
         String binarySum = "";
 
         int n = a.length(), m = b.length();        
