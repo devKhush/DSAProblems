d8d5eefbb8d6fcea4f3c23e674b06122a1ba69d0
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-05 16:28:27 +0530

    Time: 3 ms (69.40%), Space: 42.1 MB (93.11%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -1,10 +1,40 @@
 class Solution {
-    public int myAtoi(String s) {
+     public int myAtoi(String s) {
+        int number = 0;
+        int sign = +1;
+        int i = 0, n = s.length();
+
+        if (n == 0) return number;
+
+        while (i < n  &&  s.charAt(i) == ' ')
+            i++;
+
+        if (i < n  && (s.charAt(i) == '-' || s.charAt(i) == '+')){
+            sign = s.charAt(i) == '+' ? +1 : -1;
+            i++;
+        }
+
+        while (i < n){
+            int digit = s.charAt(i) - '0';
+            if (digit < 0 || digit > 9)
+                break;
+    
+            if (number > (Integer.MAX_VALUE - digit)/10)
+                return sign == +1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            
+            number = 10 * number + digit;
+            i++;
+        }
+        return sign * number;
+    }
+    
+    public int myAtoi_V1(String s) {
         long number = 0;
         int sign = +1;
         int i = 0, n = s.length();
         
-        if (n == 0) return 0;
+        if (n == 0) 
+            return 0;
         
         while (i < n  &&  s.charAt(i) == ' ')
             i++;
@@ -13,11 +43,8 @@ class Solution {
             sign = s.charAt(i) == '+' ? +1 : -1;
             i++;
         }
-
-        
         while (i < n){
             int digit = s.charAt(i) - '0';
-            
             if (digit < 0 || digit > 9)
                 break;
             
@@ -26,7 +53,7 @@ class Solution {
 
             if (sign == -1  && -number <= Integer.MIN_VALUE)
                 return Integer.MIN_VALUE;
-            else if (number > Integer.MAX_VALUE)
+            else if (sign == 1  &&  number >= Integer.MAX_VALUE)
                 return Integer.MAX_VALUE;
         }
         return (int) (sign * number);
