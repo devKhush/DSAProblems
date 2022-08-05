b5485f60675c6686d2f2ee6b2c1d51315f5a4463
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-05 13:27:42 +0530

    Time: 7 ms (83.80%), Space: 45.5 MB (59.67%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,32 +1,18 @@
 class Solution {
     public String intToRoman(int number) {
-        HashMap<Integer, String> romans = new HashMap<>();
-        romans.put(1, "I");
-        romans.put(4, "IV");
-        romans.put(5, "V");
-        romans.put(9, "IX");
-        romans.put(10, "X");
-        romans.put(40, "XL");
-        romans.put(50, "L");
-        romans.put(90, "XC");
-        romans.put(100, "C");
-        romans.put(400, "CD");
-        romans.put(500, "D");
-        romans.put(900, "CM");
-        romans.put(1000, "M");
-        
-        int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int[] romanNumber = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanString = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}; 
         
         StringBuilder roman = new StringBuilder();
         
         for (int i = 0; i < 13; i++){
-            if (number >= numbers[i]){
-                int count = number / numbers[i];
+            if (number >= romanNumber[i]){
+                int count = number / romanNumber[i];
                 
                 while (count-- > 0)
-                    roman.append(romans.get(numbers[i]));
+                    roman.append(romanString[i]);
                 
-                number %= numbers[i];
+                number %= romanNumber[i];
             }
         }
         return roman.toString();
