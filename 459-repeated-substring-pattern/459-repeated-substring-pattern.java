847e596c04067c7f8b9c2c7b3689b9b8e53e57f3
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-09 11:39:32 +0530

    Time: 126 ms (27.10%), Space: 110.4 MB (16.96%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -12,10 +12,25 @@ class Solution {
                 for (int j = 0; j < repeatTimes; j++)
                     sb.append(subString);
                 
-                if (sb.toString().equals(s))
+                if (equals(s, sb.toString()))
                     return true;
             }
         }
         return false;
+    }
+    
+    
+    private boolean equals(String s1, String s2){
+        int n = s1.length();
+        int i = 0, j = 0;
+        
+        while (i < n && j < n){
+            if (s1.charAt(i) != s2.charAt(j))
+                return false;
+            
+            i++;
+            j++;
+        }
+        return true;
     }
 }
