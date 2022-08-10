1f2172c216a6b1611402884ec75286c2d4a25dd1
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-10 16:42:12 +0530

    Time: 1 ms (52.37%), Space: 42.7 MB (15.75%) - LeetHub

---
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> postOrderTraversal = new ArrayList<>();
+    
+        // postOrder(root, postOrderTraversal);
+        if (root == null)
+            return postOrderTraversal;
+
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        s1.push(root);
+
+        while (!s1.isEmpty()){
+            TreeNode node = s1.pop();
+            s2.push(node.val);
+
+            if (node.left != null)
+                s1.push(node.left);
+
+            if (node.right != null)
+                s1.push(node.right);
+        }
+        while (!s2.isEmpty())
+            postOrderTraversal.add(s2.pop());
+
+        return postOrderTraversal;
+    }
+    
+
+    public void postOrder(TreeNode root, List<Integer> postOrderTraversal){
+        if (root!=null){
+            postOrder(root.left, postOrderTraversal);
+            postOrder(root.right, postOrderTraversal);
+            postOrderTraversal.add(root.val);
+        }
+    }
+
+}
