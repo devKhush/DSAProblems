a494db8d2adba91a6922bccc8013ce22b8ce2d90
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-14 11:51:09 +0530

    Time: 3 ms (6.88%), Space: 43 MB (36.27%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -15,6 +15,48 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+           List<List<Integer>> zigzagTraversal = new ArrayList<>();
+        if (root == null)
+            return zigzagTraversal;
+
+        boolean addFromLeftToRight = true;
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.addLast(root);
+
+        while (!deque.isEmpty()){
+            List<Integer> currLevel = new ArrayList<>();
+            zigzagTraversal.add(currLevel);
+            
+            int size = deque.size();
+            for (int i = 0; i < size; i++){
+                
+                if (addFromLeftToRight){
+                    TreeNode node = deque.removeFirst();
+                    currLevel.add(node.val);
+                    
+                    if (node.left != null)
+                        deque.addLast(node.left);
+                    if (node.right != null)
+                        deque.addLast(node.right);
+                }
+                else{
+                    TreeNode node = deque.removeLast();
+                    currLevel.add(node.val);
+                    
+                    if (node.right != null)
+                        deque.addFirst(node.right);
+                    if (node.left != null)
+                        deque.addFirst(node.left);
+                }
+            }
+            addFromLeftToRight = !addFromLeftToRight;
+        }
+        return zigzagTraversal;
+    }
+    
+    
+    // Solution 1: Not preferred Though
+    public List<List<Integer>> zigzagLevelOrder_V1(TreeNode root) {
         List<List<Integer>> zigzagTraversal = new ArrayList<>();
         if (root == null)
             return zigzagTraversal;
