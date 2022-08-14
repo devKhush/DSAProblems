82b47d34ad2e52cd055ef5d283c0a282c481ec55
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-14 11:27:39 +0530

    Time: 2 ms (38.81%), Space: 42.7 MB (63.53%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -16,42 +16,33 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> zigzagTraversal = new ArrayList<>();
-        
         if (root == null)
             return zigzagTraversal;
-        
+
+        boolean addNodeToLast = true;
         Queue<TreeNode> queue = new ArrayDeque<>();
         queue.add(root);
-        
+
         while (!queue.isEmpty()){
             int size = queue.size();
             List<Integer> currLevel = new ArrayList<>();
             zigzagTraversal.add(currLevel);
-            
+
             while (size-- > 0){
                 TreeNode node = queue.remove();
-                currLevel.add(node.val);
-                
+
+                if (addNodeToLast)
+                    currLevel.add(node.val);
+                else
+                    currLevel.add(0, node.val);
+
                 if (node.left != null)
                     queue.add(node.left);
                 if (node.right != null)
                     queue.add(node.right);
             }
+            addNodeToLast = !addNodeToLast;
         }
-        
-        for (int i = 1; i < zigzagTraversal.size(); i += 2)
-            reverse(zigzagTraversal.get(i));
-        
         return zigzagTraversal;
-    }
-    
-    private void reverse(List<Integer> arr){
-        int n = arr.size();
-        
-        for (int i = 0; i < n/2; i++){
-            int temp = arr.get(i);
-            arr.set(i, arr.get(n - 1 - i));
-            arr.set(n - 1 - i, temp);
-        }
     }
 }
