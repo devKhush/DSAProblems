ff0024ffc44bd961a33dee0c984ebaec671e8c8f
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-15 15:51:48 +0530

    Time: 2 ms (12.65%), Space: 41.8 MB (78.30%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,11 +15,42 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null)
-            return true;
+        // return checkSymmetric(root.left, root.right);        
         
-        return checkSymmetric(root.left, root.right);
+         if (root == null)
+            return true;
+
+        if (root.left == null  &&  root.right == null)
+            return true;
+
+        if (root.left == null  || root.right == null)
+            return false;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()){
+            TreeNode node1 = queue.remove();
+            TreeNode node2 = queue.remove();
+
+            if (node1 == null && node2 == null)
+                continue;
+
+            if (node1 == null || node2 == null)
+                return false;
+
+            if (node1.val != node2.val)
+                return false;
+            
+            queue.add(node1.right);
+            queue.add(node2.left);
+            queue.add(node1.left);
+            queue.add(node2.right);
+        }
+        return true;
     }
+    
     
     public boolean checkSymmetric(TreeNode A, TreeNode B){
         if (A == null && B == null)
