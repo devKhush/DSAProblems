417fd6bc36b005d4c33bb7360515278e94207c0b
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-16 15:47:11 +0530

    Time: 3 ms (12.57%), Space: 42.5 MB (72.46%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,7 +14,41 @@
  * }
  */
 class Solution {
+    // BFS Solution 1 *************************************************************************
     public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> rightSideView = new ArrayList<>();
+        if (root == null)
+            return rightSideView;
+
+        HashMap<Integer, Integer> rightSideMap = new HashMap<>();
+
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(root, 0));
+
+        while (!queue.isEmpty()){
+            Node node = queue.remove();
+
+            if (!rightSideMap.containsKey(node.level))
+                rightSideMap.put(node.level, node.treeNode.val);
+
+            if (node.treeNode.right != null)
+                queue.add(new Node(node.treeNode.right, node.level + 1));
+
+            if (node.treeNode.left != null)
+                queue.add(new Node(node.treeNode.left, node.level + 1));
+        }
+
+        int level = 0;
+        while (rightSideMap.containsKey(level)){
+            rightSideView.add(rightSideMap.get(level));
+            level++;
+        }
+        return rightSideView;
+    }
+    
+    
+    // BFS Solution 2 *************************************************************************
+    public List<Integer> rightSideView_BFS1(TreeNode root) {
         ArrayList<Integer> rightSideView = new ArrayList<>();
         if (root == null)
             return rightSideView;
@@ -26,7 +60,7 @@ class Solution {
         
         while (!queue.isEmpty()){
             Node node = queue.remove();
-            
+    
             rightSideMap.put(node.level, node.treeNode.val);
             
             if (node.treeNode.left != null)
