6c0451e4a1ecab6a65e87ec0a17a43419ccdb2af
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-17 11:56:32 +0530

    Time: 12 ms (36.58%), Space: 48.6 MB (11.41%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,14 +9,52 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        ArrayList<TreeNode> pathToNodeP = new ArrayList<>();
+        ArrayList<TreeNode> pathToNodeQ = new ArrayList<>();
+
+        findPathFromRootToNode(root, p, pathToNodeP);
+        findPathFromRootToNode(root, q, pathToNodeQ);
+
+        int minSize = Math.min(pathToNodeP.size(), pathToNodeQ.size());
+
+        for (int i = 0; i <= minSize; i++){
+            if (i == minSize)
+                return pathToNodeP.get(minSize - 1);
+            if (pathToNodeP.get(i) != pathToNodeQ.get(i))
+                return pathToNodeP.get(i - 1);
+        }
+        return null;
+    }
+
+    private boolean findPathFromRootToNode(TreeNode node, TreeNode destination, ArrayList<TreeNode> path){
+        if (node == null)
+            return false;
+
+        path.add(node);
+
+        if (node == destination)
+            return true;
+
+        if (findPathFromRootToNode(node.left, destination, path))
+            return true;
+        if (findPathFromRootToNode(node.right, destination, path))
+            return true;
+
+        path.remove(path.size() - 1);
+        return false;
+    }
+    
+    
+    
+    public TreeNode lowestCommonAncestor_(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null)
             return null;
         
         if (root == p || root == q)
             return root;
         
-        TreeNode leftResult = lowestCommonAncestor(root.left, p, q);
-        TreeNode rightResult = lowestCommonAncestor(root.right, p, q);
+        TreeNode leftResult = lowestCommonAncestor_(root.left, p, q);
+        TreeNode rightResult = lowestCommonAncestor_(root.right, p, q);
         
         if (leftResult == null)
             return rightResult;
