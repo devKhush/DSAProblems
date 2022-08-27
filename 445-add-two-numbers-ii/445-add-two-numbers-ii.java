9219f7ec19d911f61796e7c23a6649b400a2822a
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-08-27 17:59:23 +0530

    Time: 9 ms (21.59%), Space: 48.7 MB (8.60%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -10,40 +10,38 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode head1, ListNode head2) {
-        Stack<ListNode> num1 = new Stack<>();
-        Stack<ListNode> num2 = new Stack<>();
-        
+          Stack<ListNode> s1 = new Stack<>();
+        Stack<ListNode> s2 = new Stack<>();
+
         ListNode l1 = head1, l2 = head2;
-        while (l1 != null || l2 != null){
+
+        while (l1 !=  null || l2 != null){
             if (l1 != null){
-                num1.push(l1);
+                s1.push(l1);
                 l1 = l1.next;
             }
             if (l2 != null){
-                num2.push(l2);
+                s2.push(l2);
                 l2 = l2.next;
             }
         }
-        
-        int carry = 0;
+
         Stack<ListNode> result = new Stack<>();
-        
-        while (!num1.isEmpty() || !num2.isEmpty() || carry != 0){
+        result.push(null);
+        int carry = 0;
+
+        while (!s1.isEmpty() || !s2.isEmpty() || carry != 0){
             int sum = 0;
-            sum += !num1.isEmpty() ? num1.pop().val : 0;
-            sum += !num2.isEmpty() ? num2.pop().val : 0;
+            sum += !s1.isEmpty() ? s1.pop().val : 0;
+            sum += !s2.isEmpty() ? s2.pop().val : 0;
             sum += carry;
+
+            ListNode node = new ListNode(sum % 10);
+            node.next = result.peek();
             
-            result.push(new ListNode(sum % 10));
+            result.push(node);
             carry = sum / 10;
         }
-        
-        ListNode dummy = new ListNode(0), ptr = dummy;
-        
-        while (!result.isEmpty()){
-            ptr.next = result.pop();
-            ptr = ptr.next;
-        }
-        return dummy.next;
+        return result.peek();
     }
 }
