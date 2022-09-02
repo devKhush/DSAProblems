d3e80af3e328a3e3194803581da0f6b9d4fa9787
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-09-03 00:52:53 +0530

    Time: 6 ms (11.80%), Space: 47.1 MB (65.76%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -22,24 +22,42 @@ class Node {
 */
 
 class Solution {
+    // DFS *****************************************************************
     public Node connect(Node root) {
         if (root == null)
             return null;
-
+        
+        connect(root.left, root.right);
+        return root;
+    }
+    
+    public void connect(Node l1, Node l2){
+        if (l1 == null || l2 == null)
+            return;
+        
+        l1.next = l2;
+        connect(l1.left, l1.right);
+        connect(l2.left, l2.right);
+        connect(l1.right, l2.left);
+    }
+    
+    
+    // BFS *****************************************************************
+    public Node connect_BFS(Node root) {
+        if (root == null)
+            return null;
         Queue<Node> queue = new ArrayDeque<>();
         queue.add(root);
 
         while (!queue.isEmpty()){
             int size = queue.size();
-
             Node tail = null;
-
+            
             for (int i = 0; i < size; i++) {
                 Node node = queue.remove();
 
                 if (node.left != null)
                     queue.add(node.left);
-
                 if (node.right != null)
                     queue.add(node.right);
 
