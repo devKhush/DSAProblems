06d8ae7192a193cdcd48b60d6b46d9a539570ab4
Khush dev <88544100+devKhush@users.noreply.github.com>
2022-11-04 00:47:48 +0530

    Time: 3 ms (8.98%), Space: 42.4 MB (24.97%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,6 +14,39 @@
  * }
  */
 class Solution {
+    
+     public TreeNode bstFromPreorder(int[] preorder){
+        int[] inorder = new int[preorder.length];
+        System.arraycopy(preorder, 0, inorder, 0, preorder.length);
+        Arrays.sort(inorder);
+
+        HashMap<Integer, Integer> inorderMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++)
+            inorderMap.put(inorder[i], i);
+
+        return constructTree(preorder, 0, preorder.length-1,
+                              inorder,  0,  inorder.length-1,  inorderMap);
+    }
+
+    private TreeNode constructTree(int[] preorder, int preStart, int preEnd,
+                                   int[] inorder, int inStart, int inEnd,
+                                   HashMap<Integer, Integer> inorderMap){
+        if (preEnd < preStart)
+            return null;
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int rootIndex = inorderMap.get(preorder[preStart]);
+
+        root.left = constructTree(preorder, preStart + 1, preStart + (rootIndex - inStart),
+                                  inorder, inStart, rootIndex -1, inorderMap);
+        root.right = constructTree(preorder, preStart + (rootIndex - inStart) + 1, preEnd,
+                                   inorder, rootIndex + 1, inEnd, inorderMap);
+        return root;
+    }
+
+    
+    
+    // Brute Force *********************************************************************
      private TreeNode insertBST(TreeNode root, int value){
         if (root == null)
             return new TreeNode(value);
@@ -24,7 +57,7 @@ class Solution {
             root.right = insertBST(root.right, value);
         return root;
     }
-    public TreeNode bstFromPreorder(int[] preorder) {
+    public TreeNode bstFromPreorder_Brute(int[] preorder) {
         TreeNode root = null;
         for (int node : preorder)
             root = insertBST(root, node);
