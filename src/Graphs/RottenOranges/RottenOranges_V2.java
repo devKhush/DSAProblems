27385f331a2cd32322bf8b9ef596d3a3d1cd39f4
Khush dev <khushdev20211@iiitd.ac.in>
2023-03-04 16:23:30 +0530

    rotten orange bfs

---
+++ src/Graphs/RottenOranges/RottenOranges_V2.java
@@ -0,0 +1,58 @@
+package Graphs.RottenOranges;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class RottenOranges_V2 {
+    /*********************************** BFS Solution *********************************************
+     * Time Complexity: O(mn) + O(mn)  ~  O(mn)
+     * Space Complexity: O(mn)
+        * Queue size in worst case
+     */
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] visited = new int[m][n];
+
+        int[] di = { 1, -1, 0, 0 };
+        int[] dj = { 0, 0, 1, -1 };
+
+        int time = 0;
+        int freshOranges = 0, orangesRotten = 0;
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = 2;
+                }
+                if (grid[i][j] == 1)
+                    freshOranges++;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int $ = 0; $ < size; $++) {
+                int i = queue.peek()[0];
+                int j = queue.peek()[1];
+                queue.remove();
+
+                for (int a = 0; a < 4; a++) {
+                    int newI = i + di[a];
+                    int newJ = j + dj[a];
+                    if (newI >= 0 && newJ >= 0 && newI < m && newJ < n && grid[newI][newJ] == 1 && visited[newI][newJ] != 2) {
+                        visited[newI][newJ] = 2;                   // rot fresh orange
+                        orangesRotten++;
+                        queue.add(new int[] { newI, newJ });    // add to queue
+                    }
+                }
+            }
+            if (!queue.isEmpty())
+                time++;
+        }
+        // if all fresh orange couldn't be rotten, return -1
+        return freshOranges == orangesRotten ? time : -1;
+    }
+}
