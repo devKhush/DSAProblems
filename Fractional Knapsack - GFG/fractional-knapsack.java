91f6accae3929ccfeba5e32a2d16e46a2d52b63b
Khush dev <88544100+devKhush@users.noreply.github.com>
2023-04-16 15:24:57 +0530

    Added solution - LeetHub

---
+++ Fractional Knapsack - GFG/fractional-knapsack.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+
+class GfG {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int w = Integer.parseInt(inputLine[1]);
+            Item[] arr = new Item[n];
+            inputLine = br.readLine().trim().split(" ");
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Item(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            System.out.println(String.format("%.2f", new Solution().fractionalKnapsack(w, arr, n)));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+*/
+
+class Solution{
+    double fractionalKnapsack(int W, Item items[], int n){
+        Arrays.sort(items, (a,b) -> Double.compare(b.value/(double)b.weight, a.value/(double)a.weight));
+
+        double maxValues = 0;
+        for (int i = 0; i < n; i++){
+            if (W >= items[i].weight){
+                maxValues += items[i].value;
+                W -= items[i].weight;
+            }
+            else{
+                maxValues += W * (items[i].value / (double)items[i].weight);
+                break;
+            }
+        }
+        return maxValues;
+    }
+}
