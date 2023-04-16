13858824a618f562f14f9db590591d595377cb74
Khush dev <88544100+devKhush@users.noreply.github.com>
2023-04-16 16:05:59 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.java
+++ Number of Coins - GFG/number-of-coins.java
@@ -26,11 +26,25 @@ class GfG
 
 class Solution{
 	public int minCoins(int[] coins, int n, int amount) {
-	    Integer[][] dp = new Integer[n][amount + 1];
-	    
-	    int minCoins =  f(n - 1, amount, coins, n, dp);
-	    return minCoins != (int)1e9 ? minCoins : -1; 
+	   // Integer[][] dp = new Integer[n][amount + 1];
+	   // int minCoins =  f(n - 1, amount, coins, n, dp);
+	   // return minCoins != (int)1e9 ? minCoins : -1; 
+	   
+	   int[][] dp = new int[n][amount + 1];
+	   for (int amt = 0; amt <= amount; amt++)
+	        dp[0][amt] = amt % coins[0] == 0 ? amt / coins[0] : (int)1e9;
+	        
+	   for (int i = 1; i < n; i++){
+	       for (int amt = 1; amt <= amount; amt++){
+	           int take = amt >= coins[i] ? 1 + dp[i][amt - coins[i]] : (int)1e9;
+	           int notTake = 0 + dp[i - 1][amt];
+	           
+	           dp[i][amt] = Math.min(take, notTake);
+	       }
+	   }
+	   return dp[n - 1][amount] != (int)1e9 ? dp[n-1][amount] : -1;
 	} 
+	
 	
 	private int f(int i, int amount, int[] coins , int n, Integer[][] dp){
 	    if (amount == 0)
