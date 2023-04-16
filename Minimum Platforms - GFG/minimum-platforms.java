67bc083efe4d30fbebce72b0500a671da6adb653
Khush dev <88544100+devKhush@users.noreply.github.com>
2023-04-16 12:48:56 +0530

    Added solution - LeetHub

---
+++ Minimum Platforms - GFG/minimum-platforms.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            
+            int arr[] = new int[n];
+            int dep[] = new int[n];
+            
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+              arr[i] = Integer.parseInt(str[i]);
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                dep[i] = Integer.parseInt(str[i]);
+                
+            System.out.println(new Solution().findPlatform(arr, dep, n));
+        }
+    }
+    
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int findPlatform(int arr[], int dep[], int n)    {
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int departureIndex = 0, arrivalIndex = 1;
+        int platforms = 1;
+        int minPlatforms = 1;
+
+        while (arrivalIndex < n) {
+            if (arr[arrivalIndex] > dep[departureIndex]) {
+                platforms--;
+                departureIndex++;
+            } else {
+                platforms++;
+                arrivalIndex++;
+            }
+            minPlatforms = Math.max(minPlatforms, platforms);
+        }
+        return minPlatforms;
+    }
+}
+
